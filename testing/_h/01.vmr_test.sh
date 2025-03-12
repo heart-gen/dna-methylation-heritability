c92323c9129c5caa9a43df886572ea072424f7b3
Alexis Bennett <alexis.bennett@northwestern.edu>
2025-03-12 13:56:13 -0500

    Copy test scripts to working directory

---
+++ testing/_h/01.vmr_test.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#SBATCH --account=p32505        # Replace with your allocation
+#SBATCH --partition=short       # Partition (queue) name
+#SBATCH --time=01:00:00         # Time limit hrs:min:sec
+#SBATCH --nodes=1               # Number of nodes
+#SBATCH --ntasks-per-node=1     # Number of cores (CPU)
+#SBATCH --mem=1G                # Memory limit
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=alexis.bennett@northwestern.edu
+#SBATCH --job-name=vmr_test  # Job name
+#SBATCH --output=vmr_%j_out.log  # Standard output log
+#SBATCH --error=vmr_%j_err.log    # Standard error log
+
+# Log function
+log_message() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
+}
+
+log_message "**** Job starts ****"
+
+echo "**** QUEST info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOBID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURM_NODENAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID:-N/A}"
+
+## List current modules for reproducibility
+
+module purge
+module load R/4.3.0
+module list
+
+# Run VMR script 
+
+Rscript --verbose VMR_test.r 
