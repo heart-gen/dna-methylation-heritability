a49cec80568b2c3bab570eafa7d9fb9a0bf5499c
Alexis Bennett <alexis.bennett@northwestern.edu>
2025-04-08 13:05:12 -0500

    Update to include African genetic ancestry proportions

--- testing/caudate/_h/02.pca_test.R
+++ testing/caudate/_h/02.pca_test.R
@@ -21,14 +21,38 @@ meth <- as.matrix(getMeth(BS))
 id_top <- colData(BS)$brnum
 colnames(meth) <- id_top
 
-# pca
-pc = prcomp(t(meth), scale. = T, center = T)
-cumsum(summary(pc)$importance[2,1:10])
-# PC1     PC2     PC3     PC4     PC5     PC6     PC7     PC8     PC9    PC10
-#0.11994 0.16300 0.19064 0.21188 0.22916 0.24498 0.25889 0.27264 0.28560 0.29838
+# get ancestry
+f_ances <- here("inputs/genetic-ancestry/structure.out_ancestry_proportion_raceDemo_compare")
+gen_ances <- read.table(f_ances,header=T)
+idx <- match(id_top,gen_ances$id)
 
-pdf("pca.pdf")
+# residual after regressing out Afr proportion
+res <- meth
+for(i in 1:nrow(meth)){
+  if(! i %% 10000){
+    cat(i,"\n")
+  }
+  model = lm(meth[i,] ~ gen_ances$Afr[idx])
+  res[i,] = resid(model)
+}
+
+# pca
+pc = prcomp(t(res), scale. = T, center = T)
+cumsum(summary(pc)$importance[2,1:10])
+#PC1     PC2     PC3     PC4     PC5     PC6     PC7     PC8     PC9    PC10 
+#0.05648 0.08545 0.11074 0.13106 0.15105 0.16687 0.17921 0.19071 0.20140 0.21163 
+
+pdf(file=file.path(output,"pca.pdf"))
 plot(pc)
 plot(pc$x[,1], pc$x[,2])
 dev.off()
-write.csv(pc$x,"pc.csv")
+write.csv(pc$x,file=file.path(output,"pc.csv"))
+
+# correlation of pc with ancestry
+res <- matrix(NA,nrow=ncol(pc$x),ncol=2)
+for(i in 1:ncol(pc$x)){
+  tmp <- cor.test(gen_ances$Afr[idx],pc$x[,i])
+  res[i,1] <- tmp$estimate
+  res[i,2] <- tmp$p.value
+}
+write.csv(res,file=file.path(output,"pc_ances_cor.csv"))
