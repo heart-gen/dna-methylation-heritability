49e682cc16218666c57746c5524ec4b8a5163e54
Alexis Bennett <alexis.bennett@northwestern.edu>
2025-04-09 13:29:56 -0500

    Output text file with group names

--- testing/caudate/_h/04.ld_test.sh
+++ testing/caudate/_h/04.ld_test.sh
@@ -8,10 +8,11 @@ gcta64 --bfile TOPMed_LIBD.AA.VMR1 --ld-score-region 200 --out TOPMed_LIBD.AA.VM
 Rscript 05.stratify_LD.R
 
 # Make GRM for each group
-gcta64 --bfile TOPMed_LIBD.AA.VMR1 --extract snp_group1.txt --make-grm --out TOPMed_LIBD.AA.VMR1_group1
-gcta64 --bfile TOPMed_LIBD.AA.VMR1 --extract snp_group2.txt --make-grm --out TOPMed_LIBD.AA.VMR1_group2
-gcta64 --bfile TOPMed_LIBD.AA.VMR1 --extract snp_group3.txt --make-grm --out TOPMed_LIBD.AA.VMR1_group3
-gcta64 --bfile TOPMed_LIBD.AA.VMR1 --extract snp_group4.txt --make-grm --out TOPMed_LIBD.AA.VMR1_group4
+for i in 1:4 ; do
+gcta64 --bfile TOPMed_LIBD.AA.VMR1 --extract snp_group_${i}.txt --make-grm --out TOPMed_LIBD.AA.VMR1_group_${i}
+
+echo "TOPMed_LIBD.AA.VMR1_group_${i}" >> VMR1_multi_GRMs.txt
+done
 
 # GREML with multiple GRM
-gcta64 --reml --mgrm multi_GRMs.txt --pheno methylation.phen --covar TOPMed_LIBD.AA.covar --qcovar TOPMed_LIBD.AA.qcovar --out TOPMed_LIBD.AA.VMR1
+gcta64 --reml --mgrm VMR1_multi_GRMs.txt --pheno VMR1_meth.phen --covar TOPMed_LIBD.AA.covar --qcovar TOPMed_LIBD.AA.qcovar --out TOPMed_LIBD.AA.VMR1
