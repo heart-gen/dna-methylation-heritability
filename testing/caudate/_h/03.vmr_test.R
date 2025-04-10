a5f8b17e91225a6dd92c2bb09f628415a8bb25cc
Alexis Bennett <alexis.bennett@northwestern.edu>
2025-04-10 15:24:40 -0500

    Remove old code for loading in files

--- testing/caudate/_h/03.vmr_test.R
+++ testing/caudate/_h/03.vmr_test.R
@@ -10,23 +10,16 @@ library(dplyr)
 library(genio)
 library(plinkr)
 
-# read files
-#fs <- list.files(path = ".", pattern = "^p",recursive = T, full.names = T)
-#fs <- fs[!grepl("pc",fs)]
-#raw <-  lapply(fs, function(x) fread(x,data.table=F))
-#v <- rbindlist(raw)
-#colnames(v) <- c("chr","start","sd")
-
 # sd cutoff
 sdCut <- quantile(v[,3], prob = 0.99, na.rm = TRUE)
 
 # get vmr
 vmrs <- c()
-#	v2 <- v[v$chr==paste0("chr",i),] # Error
-#	v2 <- v2[order(v2$start)]
-isHigh <- rep(0, nrow(v))
-isHigh[v$sd > sdCut] <- 1
-vmrs0 <- bsseq:::regionFinder3(isHigh, as.character(v$chr), v$start, maxGap = 1000)$up
+v2 <- v[v$chr==i,] # Error
+v2 <- v2[order(v2$start),]
+isHigh <- rep(0, nrow(v2))
+isHigh[v2$sd > sdCut] <- 1
+vmrs0 <- bsseq:::regionFinder3(isHigh, as.character(v2$chr), v2$start, maxGap = 1000)$up
 vmr <- vmrs0[vmrs0$n > 5,1:3]
 write.table(vmr,file=file.path(output,"vmr.bed"),col.names=F,row.names=F,sep="\t",quote=F)
 
