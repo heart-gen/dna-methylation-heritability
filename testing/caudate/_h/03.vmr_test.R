8e41d739e986360e6927e5d92df41dda59136a23
Alexis Bennett <alexis.bennett@northwestern.edu>
2025-04-10 15:41:58 -0500

    Update to take in command line arguments to process all regions

--- testing/caudate/_h/03.vmr_test.R
+++ testing/caudate/_h/03.vmr_test.R
@@ -10,28 +10,37 @@ library(dplyr)
 library(genio)
 library(plinkr)
 
+args <- commandArgs(trailingOnly = TRUE)
+chr <- args[1]
+start_pos <- args[2]
+end_pos <- args[3]
+
+here::i_am("testing/caudate/_h/01.sd_test.R")
+output_path <- here(paste0("testing/caudate/_m/chr_",chr))
+
+# load sd of raw DNAm
+load(here(paste0("testing/caudate/_m/chr_",chr,"/stats.rda")))
+v <- data.frame(chr=chr,start=start(BSobj),sd=sds)
+
 # sd cutoff
 sdCut <- quantile(v[,3], prob = 0.99, na.rm = TRUE)
 
 # get vmr
 vmrs <- c()
-v2 <- v[v$chr==i,] # Error
+v2 <- v[v$chr==chr,]
 v2 <- v2[order(v2$start),]
 isHigh <- rep(0, nrow(v2))
 isHigh[v2$sd > sdCut] <- 1
 vmrs0 <- bsseq:::regionFinder3(isHigh, as.character(v2$chr), v2$start, maxGap = 1000)$up
 vmr <- vmrs0[vmrs0$n > 5,1:3]
-write.table(vmr,file=file.path(output,"vmr.bed"),col.names=F,row.names=F,sep="\t",quote=F)
+write.table(vmr,file=file.path(output_path,"vmr.bed"),col.names=F,row.names=F,sep="\t",quote=F)
 
-# extract methylation values for 1 vmr
-chr <- "chr1"
-start <- 903969
-end <- 904084
-reg <- GRanges(seqnames = chr, ranges = IRanges(start = start, end = end))
+# extract methylation values for each VMR
+reg <- GRanges(seqnames = chr, ranges = IRanges(start = start_pos, end = end_pos))
 meth_reg <- getMeth(BSobj, regions = reg, what="perRegion")
 
 # read in FID, IID from sample file 
-samples <- read_plink2_psam_file("/projects/p32505/projects/dna-methylation-heritability/inputs/genotypes/TOPMed_LIBD.AA.psam")
+samples <- read_plink2_psam_file(here("inputs/inputs/genotypes/TOPMed_LIBD.AA.psam"))
 samples <- samples[,1:2]
 
 # add sample IDs to methylation matrix
@@ -48,5 +57,11 @@ meth_reg_merged <- meth_reg_merged %>%
 
 # write methylation values to .phen file
 colnames(meth_reg_merged)[1:3] <- c("fam", "id", "pheno")
-write_phen(file=file.path(output,"VMR1_meth.phen"), meth_reg_merged)
+write_phen(file=file.path(output_path,"VMR1_meth.phen"), meth_reg_merged)
 
+#### Reproducibility information ####
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+sessioninfo::session_info()
