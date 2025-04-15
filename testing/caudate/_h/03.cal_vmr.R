1862d00953a942aea260ff5bae258cd5a34aaea6
Alexis Bennett <alexis.bennett@northwestern.edu>
2025-04-15 11:55:38 -0500

    Update to utilize functions

--- testing/caudate/_h/03.cal_vmr.R
+++ testing/caudate/_h/03.cal_vmr.R
@@ -9,35 +9,26 @@ library(GenomicRanges)
 library(dplyr)
 library(genio)
 library(plinkr)
+library(here)
 
 args <- commandArgs(trailingOnly = TRUE)
 chr <- args[1]
 start_pos <- args[2]
 end_pos <- args[3]
 
-here::i_am("testing/caudate/_h/01.sd_test.R")
+#here::i_am("testing/caudate/_h/03.cal_vmr.R")
 output_path <- here(paste0("testing/caudate/_m/chr_",chr))
 
-# load sd of raw DNAm
+# load raw DNAm
 load(here(paste0("testing/caudate/_m/chr_",chr,"/stats.rda")))
-v <- data.frame(chr=chr,start=start(BSobj),sd=sds)
-
-# sd cutoff
-sdCut <- quantile(v[,3], prob = 0.99, na.rm = TRUE)
-
-# get vmr
-vmrs <- c()
-v2 <- v[v$chr==chr,]
-v2 <- v2[order(v2$start),]
-isHigh <- rep(0, nrow(v2))
-isHigh[v2$sd > sdCut] <- 1
-vmrs0 <- bsseq:::regionFinder3(isHigh, as.character(v2$chr), v2$start, maxGap = 1000)$up
-vmr <- vmrs0[vmrs0$n > 5,1:3]
-write.table(vmr,file=file.path(output_path,"vmr.bed"),col.names=F,row.names=F,sep="\t",quote=F)
 
 # extract methylation values for each VMR
-reg <- GRanges(seqnames = chr, ranges = IRanges(start = start_pos, end = end_pos))
-meth_reg <- getMeth(BSobj, regions = reg, what="perRegion")
+calc_vmr_meth <- function(BSobj,chr,start_pos,end_pos) {
+  reg      <- GRanges(seqnames = chr, 
+                      ranges = IRanges(start = start_pos, end = end_pos))
+  meth_reg <- getMeth(BSobj, regions = reg, what="perRegion")
+  return(meth_reg)
+}
 
 # read in FID, IID from sample file 
 samples <- read_plink2_psam_file(here("inputs/inputs/genotypes/TOPMed_LIBD.AA.psam"))
