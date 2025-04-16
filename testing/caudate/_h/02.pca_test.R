e68b9cd23d2b29fe9a4c13d25bcfefc4196e98fc
Alexis Bennett <alexis.bennett@northwestern.edu>
2025-04-16 15:23:58 -0500

    Change minor formatting

--- testing/caudate/_h/02.pca_test.R
+++ testing/caudate/_h/02.pca_test.R
@@ -6,62 +6,90 @@ library(DelayedMatrixStats)
 library('data.table')
 
 args <- commandArgs(trailingOnly = TRUE)
-chr <- args[1]
+chr  <- args[1]
 
 ##here::i_am("testing/caudate/_h/01.sd_test.R")
-output_path <- here(paste0("testing/caudate/_m/chr_",chr))
+
+## Function
+get_top_meth <- function(BSobj, v) {
+    # get top 1M variable CpG
+    v_top  <- v[order(v$sd * -1), ]
+    v_top  <- v_top[1:10^6, ]
+    
+    # DNAm levels for top 1M
+    tmp    <- v_top[v_top$chr == chr, ]
+    BS     <- BSobj[is.element(start(BSobj), tmp$start), ]
+    meth   <- as.matrix(getMeth(BS))
+    id_top <- colData(BS)$brnum
+    colnames(meth) <- id_top
+    return(list(BS = BS, meth = meth, id_top = id_top))
+}
+
+get_ances <- function(f_ances, id_top) {
+    gen_ances <- read.table(f_ances, header=TRUE)
+    idx       <- match(id_top, gen_ances$id)
+    return(list(gen_ances = gen_ances, idx = idx))
+}
+
+res_ances <- function(meth, gen_ances, idx) {
+    res <- meth
+    for(i in 1:nrow(meth)) {
+      if (i %% 10000 == 0) {
+        cat(i, "\n")
+      }
+      model    <- lm(meth[i, ] ~ gen_ances$Afr[idx])
+      res[i, ] <- resid(model)
+    }
+    return(res)
+}
+
+pca <- function(res, output_path) {
+    pc <- prcomp(t(res), scale. = TRUE, center = TRUE)
+    print(cumsum(summary(pc)$importance[2,1:10]))
+    pdf(file = file.path(output_path, "pca.pdf"))
+    plot(pc)
+    plot(pc$x[, 1], pc$x[, 2])
+    dev.off()
+    write.csv(pc$x, file = file.path(output_path, "pc.csv"))
+    return(pc)
+}
+
+corr_pc_ances <- function(pc, gen_ances, idx, output_path) {
+    res <- matrix(NA, nrow=ncol(pc$x), ncol=2)
+    colnames(res) <- c("Correlation", "p-value")
+    for(i in 1:ncol(pc$x)){
+      tmp       <- cor.test(gen_ances$Afr[idx], pc$x[, i])
+      res[i, 1] <- tmp$estimate
+      res[i, 2] <- tmp$p.value
+    }
+    write.csv(res, file = file.path(output_path, "pc_ances_cor.csv"))
+    return(res)
+}
+
+## Main
+
+output_path <- here(paste0("testing/caudate/_m/chr_", chr))
 
 # load sd of raw DNAm
-load(here(paste0("testing/caudate/_m/chr_",chr,"/stats.rda")))
-v <- data.frame(chr=chr,start=start(BSobj),sd=sds)
+load(here(paste0("testing/caudate/_m/chr_", chr, "/stats.rda")))
+v <- data.frame(chr = chr, start = start(BSobj), sd = sds)
 
-# get top 1M variable CpG
-v_top <- v[order(v$sd * -1),]
-v_top <- v_top[1:10^6,]
-
-# DNAm levels for top 1M
-tmp <- v_top[v_top$chr==chr,]
-BS <- BSobj[is.element(start(BSobj),tmp$start),]
-meth <- as.matrix(getMeth(BS))
-
-id_top <- colData(BS)$brnum
-colnames(meth) <- id_top
+meth_top <- get_top_meth(BSobj, v)
 
 # get ancestry
 f_ances <- here("inputs/genetic-ancestry/structure.out_ancestry_proportion_raceDemo_compare")
-gen_ances <- read.table(f_ances,header=T)
-idx <- match(id_top,gen_ances$id)
+ances   <- get_ances(f_ances, meth_top$id_top)
 
 # residual after regressing out Afr proportion
-res <- meth
-for(i in 1:nrow(meth)){
-  if(! i %% 10000){
-    cat(i,"\n")
-  }
-  model = lm(meth[i,] ~ gen_ances$Afr[idx])
-  res[i,] = resid(model)
-}
+res <- res_ances(meth_top$meth, ances$gen_ances, ances$idx)
 
-# pca
-pc = prcomp(t(res), scale. = T, center = T)
-cumsum(summary(pc)$importance[2,1:10])
+# perform pca
+pc <- pca(res, output_path)
 #PC1     PC2     PC3     PC4     PC5     PC6     PC7     PC8     PC9    PC10 
 #0.05648 0.08545 0.11074 0.13106 0.15105 0.16687 0.17921 0.19071 0.20140 0.21163 
 
-pdf(file=file.path(output,"pca.pdf"))
-plot(pc)
-plot(pc$x[,1], pc$x[,2])
-dev.off()
-write.csv(pc$x,file=file.path(output_path,"pc.csv"))
-
 # correlation of pc with ancestry
-res <- matrix(NA,nrow=ncol(pc$x),ncol=2)
-for(i in 1:ncol(pc$x)){
-  tmp <- cor.test(gen_ances$Afr[idx],pc$x[,i])
-  res[i,1] <- tmp$estimate
-  res[i,2] <- tmp$p.value
-}
-write.csv(res,file=file.path(output_path,"pc_ances_cor.csv"))
+res <- corr_pc_ances(pc, ances$gen_ances, ances$idx, output_path)
 
 #### Reproducibility information ####
 print("Reproducibility information:")
--- testing/caudate/_h/03.cal_vmr.R
+++ testing/caudate/_h/03.cal_vmr.R
@@ -19,8 +19,8 @@ start_pos <- as.integer(args[2])
 end_pos   <- as.integer(args[3])
 
 #here::i_am("testing/caudate/_h/03.cal_vmr.R")
-
-calc_vmr_meth <- function(BSobj,chr,start_pos,end_pos) {
+## Function
+calc_vmr_meth <- function(BSobj, chr, start_pos, end_pos) {
   reg      <- GRanges(seqnames = chr, 
                       ranges = IRanges(start = start_pos, end = end_pos))
   meth_reg <- getMeth(BSobj, regions = reg, what="perRegion")
@@ -29,11 +29,11 @@ calc_vmr_meth <- function(BSobj,chr,start_pos,end_pos) {
 
 ## Main
 # load raw DNAm
-load(here("testing/caudate/_m", paste0("chr_",chr_num,"/stats.rda")))
-output_path <- here("testing/caudate/_m", paste0("chr_",chr_num))
+load(here("testing/caudate/_m", paste0("chr_", chr_num, "/stats.rda")))
+output_path <- here("testing/caudate/_m", paste0("chr_", chr_num))
 
 # extract methylation values for each VMR
-meth_reg <- calc_vmr_meth(BSobj,chr,start_pos,end_pos)
+meth_reg <- calc_vmr_meth(BSobj, chr, start_pos, end_pos)
   
 # read in FID, IID from sample file
 psam_file <- here("inputs/genotypes/TOPMed_LIBD.AA.psam")
@@ -42,7 +42,7 @@ samples   <- extract_fid_iid(psam_file)
 # merge methylation values with FID and
 # IID and write to .phen file
 out_phen    <- file.path(output_path, 
-                         paste0(start_pos,"_",end_pos,"_meth.phen"))
+                         paste0(start_pos, "_", end_pos, "_meth.phen"))
 meth_merged <- write_meth_to_phen(BSobj, meth_reg, samples, out_phen)
 
 #### Reproducibility information ####
