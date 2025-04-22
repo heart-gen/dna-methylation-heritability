b457a45bfa2bace22e60803c7338a91d2db29902
Alexis Bennett <alexis.bennett@northwestern.edu>
2025-04-22 15:46:41 -0500

    Update to process all regions using command line args

--- testing/caudate/_h/06.summary.py
+++ testing/caudate/_h/06.summary.py
@@ -1,8 +1,12 @@
 import pandas as pd
+from pyhere import here
+import os
+import argparse 
+import session_info
 
-def process_hsq_file(input_file_path, chr, start_pos, end_pos, output_file_path):
+def process_hsq_file(input, chr, start_pos, end_pos, output):
     # Read in .hsq file
-    df = pd.read_csv(input_file_path, sep='\t', header=None, names=['source', 'variance', 'standard_error'])
+    df = pd.read_csv(input, sep='\t', header=None, names=['source', 'variance', 'standard_error'])
     extracted_data = df.iloc[1:5, -2:]
     transposed_data = extracted_data.values.flatten()
 
@@ -12,16 +16,25 @@ def process_hsq_file(input_file_path, chr, start_pos, end_pos, output_file_path)
                                                 'Variance_Ve', 'Standard_Error_Ve',
                                                 'Variance_Vp', 'Standard_Error_Vp',
                                                 'Variance_Vg_Vp', 'Standard_Error_Vg_Vp'])
-    greml_df.to_csv(output_file_path, sep='\t', index=False)
+    greml_df.to_csv(output, sep='\t', index=False)
 
     return greml_df
 
-# Test run
-input_file_path = '/projects/p32505/projects/dna-methylation-heritability/testing/_m/TOPMed_LIBD.AA.VMR1.hsq'
-output_file_path = '/projects/p32505/projects/dna-methylation-heritability/testing/_m/TOPMed_LIBD.AA.VMR1.txt'
-chr = 'chr1'
-start_pos = 403969
-end_pos = 1404084
+def main():
+    parser = argparse.ArgumentParser(description='Write GREML results to csv for all regions')
+    parser.add_argument('--chr', required=True)
+    parser.add_argument('--start_pos', required=True)
+    parser.add_argument('--end_pos', required=True)
+    args = parser.parse_args()
+    
+    input = here('testing', 'caudate', '_m', 'h2')
+    output = here('testing', 'caudate', '_m', 'summary')
 
-result_df = process_hsq_file(input_file_path, chr, start_pos, end_pos, output_file_path)
-print(result_df)
+    if not os.path.exists('./summary'):
+        os.makedirs('./summary')
+
+    result_df = process_hsq_file(input, args.chr, args.start_pos, args.end_pos, output)
+    session_info.show()
+
+if __name__ == '__main__':
+    main()
