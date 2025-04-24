c45022e73c56b0ea4be20de3972518b3c80d74b0
Alexis Bennett <alexis.bennett@northwestern.edu>
2025-04-24 11:34:22 -0500

    Update formatting for comments

--- testing/caudate/_h/07.combine_results.py
+++ testing/caudate/_h/07.combine_results.py
@@ -5,14 +5,19 @@ from pyhere import here
 import session_info
 
 def merge_csv(input_dir, output_dir):
+
+                                        # recursively find all .csv files
     csv_files = glob.glob(os.path.join(input_dir, "**", "*csv"), recursive=True)
 
+                                        # read each csv into list
     df = []
     for file in csv_files:
         df_tmp = pd.read_csv(file, sep='\t')
-        df.append(df_tmp)
 
+                                        # merge all files into df
     combined_csv = pd.concat(df, ignore_index=True)
+
+                                        # write to file
     combined_csv.to_csv(os.path.join(output_dir, 'greml_summary.csv'), 
                         sep=',', index=False)
 
