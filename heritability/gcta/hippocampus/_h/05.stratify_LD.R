6471e862af234d70c230b1a0f5632d186455335e
Alexis Bennett <alexis.bennett@northwestern.edu>
2025-04-28 14:23:35 -0500

    Add scripts for dlpfc and hippocampus

---
+++ heritability/gcta/hippocampus/_h/05.stratify_LD.R
@@ -0,0 +1,100 @@
+#### Stratify SNPs by LD score of individual variants ####
+
+suppressPackageStartupMessages({
+    library(here)
+    library(ggplot2)
+})
+
+                                        # get loci from command line
+args      <- commandArgs(trailingOnly = TRUE)
+chr_num   <- args[1]
+chr       <- paste0("chr", chr_num)
+start     <- as.integer(args[2])
+end       <- as.integer(args[3])
+
+## Function
+get_snp_groups <- function(lds_seg) {
+    quartiles <- summary(lds_seg$ldscore_SNP)
+    snp_groups <- list(
+        lb1 = which(lds_seg$ldscore_SNP <= quartiles[2]),
+        lb2 = which(lds_seg$ldscore_SNP > quartiles[2] & lds_seg$ldscore_SNP <= quartiles[3]),
+        lb3 = which(lds_seg$ldscore_SNP > quartiles[3] & lds_seg$ldscore_SNP <= quartiles[5]),
+        lb4 = which(lds_seg$ldscore_SNP > quartiles[5])
+    )
+    return(list(quartiles = quartiles, snp_groups = snp_groups))
+}
+
+extract_snps <- function(lds_seg, snp_groups, out_ld, start, end) {
+    snps <- list(
+      lb1_snp = lds_seg$SNP[snp_groups$lb1],
+      lb2_snp = lds_seg$SNP[snp_groups$lb2],
+      lb3_snp = lds_seg$SNP[snp_groups$lb3],
+      lb4_snp = lds_seg$SNP[snp_groups$lb4]
+    )
+    
+    write.table(snps$lb1_snp, 
+                file = file.path(out_ld, paste0(start, "_", end, "_", 
+                "snp_group_1.txt")), 
+                row.names = F, quote = F, col.names = F)
+    write.table(snps$lb2_snp, 
+                file = file.path(out_ld, paste0(start, "_", end, "_","snp_group_2.txt")), 
+                row.names = F, quote = F, col.names = F)
+    write.table(snps$lb3_snp, 
+                file = file.path(out_ld, paste0(start, "_", end, "_","snp_group_3.txt")), 
+                row.names = F, quote = F, col.names = F)
+    write.table(snps$lb4_snp, 
+                file = file.path(out_ld, paste0(start, "_", end, "_","snp_group_4.txt")), 
+                row.names = F, quote = F, col.names = F)
+    return(snps)
+}
+
+plot_ld <- function(lds_seg, quartiles, chr_num, out_ld) {
+    
+                                        # create output dir if it
+                                        # doesn't exist
+    hist_dir <- file.path(out_ld, "hist")
+    if (!dir.exists(hist_dir)) {
+      dir.create(hist_dir)
+    }
+
+    hist <- ggplot(lds_seg, aes(x = ldscore_SNP)) + 
+        geom_histogram(binwidth = 0.1, fill = "lightblue", color = "black", alpha = 0.7) +
+        geom_vline(aes(xintercept = quartiles[2]), color = "red", linetype = "dashed") +
+        geom_vline(aes(xintercept = quartiles[3]), color = "red", linetype = "dashed") +  
+        geom_vline(aes(xintercept = quartiles[5]), color = "red", linetype = "dashed") +  
+        labs(title = paste("Chr:", chr_num, ":", start, ":", end, "SNP LD Scores"),
+            x = "SNP LD Score", y = "Frequency")
+
+                                        # write plot to file
+    pdf(file=file.path(hist_dir, paste0(start, "_", end, "_ld_hist.pdf")))
+    print(hist)
+    dev.off()
+    return(hist)
+}
+
+## Main
+
+                                        # load LD scores from GCTA
+lds_seg = read.table(
+  here("heritability/gcta/hippocampus/_m/h2", 
+      paste0("chr_", chr_num, "/TOPMed_LIBD.AA.", start, "_", end, ".score.ld")),
+  header = TRUE,
+  colClasses = c("character", rep("numeric", 8))
+)
+out_ld <- here("heritability/gcta/hippocampus/_m/h2", paste0("chr_", chr_num))
+
+                                        # stratify SNPs based on LD
+groups <- get_snp_groups(lds_seg)
+
+                                        # write SNP groups to file
+snps <- extract_snps(lds_seg, groups$snp_groups, out_ld, start, end)
+
+                                        # plot histogram of LD scores
+hist <- plot_ld(lds_seg, groups$quartiles, chr_num, out_ld)
+
+#### Reproducibility information ####
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+sessioninfo::session_info()
