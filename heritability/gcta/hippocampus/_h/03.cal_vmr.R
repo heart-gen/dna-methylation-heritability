93ae1eb3851433f56634642303dfe7b264c0ee29
Alexis Bennett <alexis.bennett@northwestern.edu>
2025-04-28 14:23:35 -0500

    Add scripts for dlpfc and hippocampus

---
+++ heritability/gcta/hippocampus/_h/03.cal_vmr.R
@@ -0,0 +1,81 @@
+#### Calculate methylation values for variably methylated regions ####
+
+suppressPackageStartupMessages({
+    library('bsseq')
+    library('HDF5Array')
+    library(DelayedMatrixStats)
+    library('data.table')
+    library(scales)
+    library(GenomicRanges)
+    library(dplyr)
+    library(genio)
+    library(plinkr)
+    library(here)
+})
+
+                                        # get loci from command line
+args      <- commandArgs(trailingOnly = TRUE)
+chr_num   <- args[1]
+chr       <- paste0("chr", chr_num)
+start_pos <- as.integer(args[2])
+end_pos   <- as.integer(args[3])
+
+## Function
+calc_vmr_meth <- function(BSobj, chr, start_pos, end_pos) {
+  reg      <- GRanges(seqnames = chr, 
+                      ranges = IRanges(start = start_pos, end = end_pos))
+  meth_reg <- getMeth(BSobj, regions = reg, what="perRegion")
+  return(meth_reg)
+}
+
+extract_fid_iid <- function(psam_file) {
+    samples <- read_plink2_psam_file(psam_file)
+    samples <- samples[, 1:2]
+    return(samples)
+}
+
+write_meth_to_phen <- function(BSobj, M, samples, out_phen) {
+
+                                        # add sample IDs to methylation matrix
+    sample_ids <- colData(BSobj)$brnum
+    meth_df    <- data.frame(FID = sample_ids, t(M))
+
+                                        # merge methylation data and 
+                                        # sample ids by FID
+    meth_merged <- meth_df %>%
+        inner_join(samples, by = "FID") %>%
+        arrange(match(FID, samples$FID)) %>%
+        select(FID, IID, everything())
+    colnames(meth_merged)[1:3] <- c("fam", "id", "pheno")
+
+                                        # write methylation values to .phen file
+    write_phen(file=out_phen, meth_merged)
+    return(meth_merged)
+}
+
+## Main
+
+                                        # load raw DNAm
+load(here("heritability/gcta/hippocampus/_m/cpg", paste0("chr_", chr_num, "/stats.rda")))
+output_path <- here("heritability/gcta/hippocampus/_m/vmr", paste0("chr_", chr_num))
+
+                                        # calculate DNAm for each VMR
+meth_reg <- calc_vmr_meth(BSobj, chr, start_pos, end_pos)
+
+                                        # read FID, IID from sample file
+psam_file <- here("inputs/genotypes/TOPMed_LIBD.AA.psam")
+samples   <- extract_fid_iid(psam_file)
+
+                                        # merge DNAm with FID, IID
+out_phen    <- file.path(output_path, 
+                         paste0(start_pos, "_", end_pos, "_meth.phen"))
+
+                                        # write to .phen file
+meth_merged <- write_meth_to_phen(BSobj, meth_reg, samples, out_phen)
+
+#### Reproducibility information ####
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+sessioninfo::session_info()
---
+++ heritability/gcta/hippocampus/_h/step_1.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#SBATCH --account=p32505        # Replace with your allocation
+#SBATCH --partition=short       # Partition (queue) name
+#SBATCH --time=01:00:00         # Time limit hrs:min:sec
+#SBATCH --nodes=1               # Number of nodes
+#SBATCH --ntasks-per-node=1     # Number of cores (CPU)
+#SBATCH --mem=10G               # Memory limit
+#SBATCH --mail-type=FAIL
+#SBATCH --array=1-22
+#SBATCH --mail-user=alexis.bennett@northwestern.edu
+#SBATCH --job-name=extract_vmr  # Job name
+#SBATCH --output=/dev/null      # Standard output log
+#SBATCH --error=/dev/null       # Standard error log
+
+# Create log directories for each chr
+LOG_DIR="logs/chr_${SLURM_ARRAY_TASK_ID}"
+mkdir -p "$LOG_DIR"
+
+# Redirect output and error logs to chr-specific log files
+exec > >(tee -a "$LOG_DIR/extract_vmr_${SLURM_ARRAY_TASK_ID}_out.log")
+exec 2> >(tee -a "$LOG_DIR/extract_vmr_${SLURM_ARRAY_TASK_ID}_err.log" >&2)
+
+# Log function
+log_message() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
+}
+
+log_message "**** Job starts ****"
+
+echo "**** QUEST info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOBID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURM_NODENAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+## List current modules for reproducibility
+
+module purge
+module list 
+
+# Set path variables
+ENV_PATH="/projects/p32505/opt/env"
+
+echo "Working on: Chromosome "$SLURM_ARRAY_TASK_ID
+
+## Activate conda environment
+#source /projects/p32505/opt/miniforge3/etc/profile.d/conda.sh
+conda run -p $ENV_PATH/R_env Rscript ../_h/01.extract_vmr.R $SLURM_ARRAY_TASK_ID
+if [ $? -ne 0 ]; then
+    log_message "Error: Conda or script execution failed"
+    exit 1
+fi
+
+log_message "**** Job ends ****"
