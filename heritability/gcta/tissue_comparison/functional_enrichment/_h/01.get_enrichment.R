ef61e3449bda009ced11f49c97170167d41ade19
Alexis Bennett <alexis.bennett@northwestern.edu>
2025-05-14 14:32:28 -0500

    Remove KEGG enrichment and run for all filter combinations

--- heritability/gcta/tissue_comparison/functional_enrichment/_h/01.get_enrichment.R
+++ heritability/gcta/tissue_comparison/functional_enrichment/_h/01.get_enrichment.R
@@ -1,54 +1,108 @@
-##### Performs GO and KEGG enrichment analysis for all brain regions #####
+##### Performs GO enrichment analysis for all brain regions #####
 suppressPackageStartupMessages({
-    library(rGREAT) 
+    library(rGREAT)
     library(here)
     library(dplyr)
+    library(purrr)
 })
 
-## Function
-get_enrichment <- function(tissue, enrichment_type, heritability_filter) {
-    # Read in summary table 
-    vmr <- read.table(here("heritability/gcta/", paste0(region, "/_m/summary/greml_summary.tsv"), sep="\t", header=TRUE))
+# Function
+filter_heritability <- function(tissue,
+                                heritability_filter = "all",
+                                pval_filter = NULL,
+                                apply_h2_filter = FALSE) {
+    # Read in summary table
+    vmr <- read.table(here("heritability/gcta/",
+                           paste0(tissue, "/_m/summary/greml_summary.tsv")),
+                     sep = "\t", header = TRUE)
 
     # Filter by heritability
-    if (heritability_filter == "heritable") {
-        vmr_filtered <- vmr %>% filter(Sum.of.V.G._Vp_Variance > 0.05)
-    } else if (heritability_filter == "non_heritable") {
-        vmr_filtered <- vmr %>% filter(Sum.of.V.G._Vp_Variance <= 0.05)
-    } else {
-        vmr_filtered <- vmr
+    if (apply_h2_filter) {
+        top_10  <- quantile(vmr$Sum.of.V.G._Vp_Variance, 0.90, na.rm = TRUE)
+        if (heritability_filter == "heritable") {
+            vmr <- vmr %>% dplyr::filter(Sum.of.V.G._Vp_Variance >= top_10)
+        } else if (heritability_filter == "non_heritable") {
+            vmr <- vmr %>% dplyr::filter(Sum.of.V.G._Vp_Variance < top_10)
+        }
     }
 
-    colnames(vmr_filtered) <- c("seqnames","start","end")
-    vmr <- plyranges::as_granges(vmr_filtered)
-   
-    if (enrichment_type == "go") {
-        # GO enrich
-        res <- great(vmr, "GO:BP", "RefSeq:hg38", background=as.character(1:22))
-        tb$GO  <- getEnrichmentTable(res)
-    } else if (enrichment_type == "KEGG") {
-        # KEGG enrich
-        res <- great(vmr, "kegg", "RefSeq:hg38", background=as.character(1:22))
-        tb$KEGG <- getEnrichmentTable(res)
+    # Filter by p-value
+    if (pval_filter != "NULL") {
+        if (heritability_filter == "heritable") {
+            vmr <- vmr %>% dplyr::filter(Pval_Variance < pval_filter)
+        } else if (heritability_filter == "non_heritable") {
+            vmr <- vmr %>% dplyr::filter(Pval_Variance > pval_filter)
+        }
     }
 
-    write.csv(tb[[enrichment_type]], paste0(region, "_", heritability_filter, "_", enrichment_type, ".csv"), row.names = FALSE)
+    return(vmr)
 }
 
-## Main
-enrichment <- c("go", "kegg")
-tissues <- c("caudate", "dlpfc", "hippocampus")
-heritability <- c("all", "heritable", "non_heritable")
+# GO enrichment
+get_enrichment <- function(vmr_filtered, tissue, filter_label) {
+    colnames(vmr_filtered) <- c("seqnames", "start", "end")
+    vmr <- plyranges::as_granges(vmr_filtered)
+    res <- great(vmr, "GO:BP", "RefSeq:hg38", background = as.character(1:22))
+    tb  <- getEnrichmentTable(res)
+    write.csv(
+        tb,
+        file = here("heritability/gcta/tissue_comparison/functional_enrichment/_m",
+                    paste0(tissue, "_", filter_label, "_GO.csv")),
+        row.names = FALSE
+    )
+}
 
-for (tissue in tissues) {
-  for (enrichment_type in enrichment) {
-    for (heritability_filter in heritability) {
-      get_enrichment(tissue, enrichment_type, heritability_filter)
+# Main
+tissues              <- c("caudate", "dlpfc", "hippocampus")
+heritability_filters <- c("all", "heritable", "non_heritable")
+pval_filters         <- c("NULL", 0.05)
+apply_h2_options     <- c(TRUE, FALSE)
+
+# Create all possible combinations
+filter_grid <- expand.grid(
+    hfilter          = heritability_filters,
+    pval             = pval_filters,
+    h2               = apply_h2_options,
+    stringsAsFactors = FALSE
+) %>%
+    # Filter out invalid combinations
+    dplyr::filter(
+        !(hfilter == "all" & (pval != "NULL" | h2 != FALSE)),
+        !(hfilter %in% c("heritable", "non_heritable") & pval == "NULL" & h2 == FALSE)
+    ) %>%
+    # Get labels for each combination
+    rowwise() %>%
+    mutate(
+        label = {
+            parts <- c(hfilter)
+        if (h2) {
+            parts <- c(parts, "h2")
+        }
+        if (pval != "NULL") {
+            parts <- c(parts, "pval")
+        }
+        paste(parts, collapse = "_")
+      }
+    ) %>%
+    ungroup()
+
+# Run analysis
+purrr::pwalk(filter_grid, function(hfilter, pval, h2, label) {
+  for (tissue in tissues) {
+    message("Running enrichment: ", tissue, " - ", label)
+  
+    # Apply the heritability and p-value filter
+    vmr_filtered <- filter_heritability(tissue, hfilter, pval, h2)
+    # Get enrichment for remaining data after filtering
+    if (nrow(vmr_filtered) > 0) {
+      get_enrichment(vmr_filtered, tissue, label)
+    } else {
+      message("No data left after filtering for ", tissue, " - ", label)
     }
   }
-}
+})
 
-#### Reproducibility information
+# Reproducibility info
 Sys.time()
 proc.time()
 options(width = 120)
