a287743f11678a2c269e3923a0f52a7e87fbf5dd
Elisa Johnson <elisajohnson2027@u.northwestern.edu>
2025-05-20 10:50:49 -0500

    changes original directory to home directory

--- testing/dlpfc/_h/step_0.sh
+++ testing/dlpfc/_h/step_0.sh
@@ -4,13 +4,16 @@
 #SBATCH --time=01:00:00         # Time limit hrs:min:sec
 #SBATCH --nodes=1               # Number of nodes
 #SBATCH --ntasks-per-node=1     # Number of cores (CPU)
-#SBATCH --mem=10G               # Memory limit
+#SBATCH --mem=25G               # Memory limit
 #SBATCH --mail-type=FAIL
 #SBATCH --array=1-22
 #SBATCH --mail-user=elisajohnson2027@u.northwestern.edu
-#SBATCH --job-name=extract_vmr  # Job name
+#SBATCH --job-name=BiocMAP  # Job name
 #SBATCH --output=extract_vmr_%j.log      # Standard output log
 #SBATCH --error=error_%j.log       # Standard error log
+#SBATCH -q shared
+#SBATCH -o ./run_first_half_jhpce.log
+#SBATCH -e ./run_first_half_jhpce.log
 
 # Log function
 log_message() {
@@ -30,19 +33,11 @@ echo "Task id: ${SLURM_ARRAY_TASK_ID}"
 ## List current modules for reproducibility
 
 module purge
-module list 
-
-#!/bin/bash
-
-#SBATCH -q shared
-#SBATCH --mem=25G
-#SBATCH --job-name=BiocMAP
-#SBATCH -o ./run_first_half_jhpce.log
-#SBATCH -e ./run_first_half_jhpce.log
+module list
 
 #  After running 'install_software.sh', this should point to the directory
 #  where BiocMAP was installed, and not say "$PWD"
-ORIG_DIR=$PWD
+ORIG_DIR=/home/uyb5533
 
 module load nextflow/20.01.0
 export _JAVA_OPTIONS="-Xms8g -Xmx10g"
