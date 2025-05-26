fb70cdf8bc1cc9776f41b82758465ab16d886097
Kynon J Benjamin <kj.benjamin90@gmail.com>
2025-05-25 20:54:50 -0400

    updated to work with limitations of array job submissions

---
+++ heritability/elastic_net_model/caudate/_h/elastic_h2_array-01.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#SBATCH --partition=RM-shared
+#SBATCH --job-name=elastic_h2
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=kynon.benjamin@northwestern.edu
+#SBATCH --output=logs/elastic_h2_%A_%a.log
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=00:15:00
+
+log_message() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
+}
+
+log_message "**** Job starts ****"
+
+echo "**** BRIDGES info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOBID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURM_NODENAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+module purge
+module load anaconda3/2024.10-1
+module list
+
+ENV_PATH="/ocean/projects/bio250020p/shared/opt/env"
+export region="caudate"
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export MKL_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+log_message "**** Run elastic net ****"
+conda run -p "${ENV_PATH}/R_env" Rscript ../_h/01.elastic-net.R
+
+log_message "**** Job ends ****"
+
