760eaf514a701080ee11732194890af8898f0b1d
KrotosBenjamin <kj.benjamin90@gmail.com>
2025-05-27 09:50:42 -0500

    updated for correct background, added additional gene sets to test

--- heritability/gcta/tissue_comparison/functional_enrichment/_h/01.get_enrichment.R
+++ heritability/gcta/tissue_comparison/functional_enrichment/_h/01.get_enrichment.R
@@ -10,65 +10,80 @@ suppressPackageStartupMessages({
     library(UniProtKeywords)
 })
 
-# Function
+## --- Function --- ##
 filter_heritability <- function(tissue,
                                 heritability_filter = "all",
                                 pval_filter = NULL,
                                 apply_h2_filter = FALSE) {
-    # Read in summary table
-    vmr <- read.table(here("heritability/gcta/",
-                           paste0(tissue, "/_m/summary/greml_summary.tsv")),
-                     sep = "\t", header = TRUE)
-    
-    # Apply FDR correction
+                                        # Read in summary table
+    vmr_test <- "/projects/p32505/users/alexis/projects/dna-methylation-heritability/heritability/gcta/caudate/_m/summary_to_del/greml_summary.tsv"
+    vmr_file <- here("heritability/gcta", tissue,
+                     "_m/summary/greml_summary.tsv")
+    vmr <- read.table(vmr_test, sep = "\t", header = TRUE)
+
+                                        # Apply FDR correction
     vmr$Pval_Variance <- p.adjust(vmr$Pval_Variance, method = "fdr")
-    
-    # Filter by heritability
+
+                                        # Filter by heritability
     if (apply_h2_filter) {
         top_10  <- quantile(vmr$Sum.of.V.G._Vp_Variance, 0.90, na.rm = TRUE)
         if (heritability_filter == "heritable") {
-            vmr <- vmr %>% dplyr::filter(Sum.of.V.G._Vp_Variance >= top_10)
+            vmr <- dplyr::filter(vmr, Sum.of.V.G._Vp_Variance >= top_10)
         } else if (heritability_filter == "non_heritable") {
-            vmr <- vmr %>% dplyr::filter(Sum.of.V.G._Vp_Variance < top_10)
+            vmr <- dplyr::filter(vmr, Sum.of.V.G._Vp_Variance < top_10)
         }
     }
 
-    # Filter by p-value
+                                        # Filter by p-value
     if (pval_filter != "NULL") {
         if (heritability_filter == "heritable") {
-            vmr <- vmr %>% dplyr::filter(Pval_Variance < pval_filter)
+            vmr <- dplyr::filter(vmr, Pval_Variance < pval_filter)
         } else if (heritability_filter == "non_heritable") {
-            vmr <- vmr %>% dplyr::filter(Pval_Variance > pval_filter)
+            vmr <- dplyr::filter(vmr, Pval_Variance > pval_filter)
         }
     }
 
     return(vmr)
 }
 
-# GO enrichment
+## --- GO enrichment --- ##
+load_vmr_background <- function(tissue) {
+                                        # Load the regions tested as background
+    vmr_file <- here("heritability/gcta", tissue, "_m/vmr_list.txt")
+    vmr_df   <- read.table(vmr_file)
+    colnames(vmr_df) <- c("seqnames", "start", "end")
+    vmr_gr <- plyranges::as_granges(vmr_df)
+    seqlevels(vmr_gr) <- paste0("chr", seqlevels(vmr_gr))
+    return(vmr_gr)
+}
+
 get_enrichment <- function(vmr_filtered, tissue, filter_label) {
     colnames(vmr_filtered) <- c("seqnames", "start", "end")
     vmr <- plyranges::as_granges(vmr_filtered)
     seqlevels(vmr) <- paste0("chr", seqlevels(vmr))
-    
+
     gene_sets <- list(
-      "GO:BP" = "GO:BP",
-      KEGG = split(gsub("hsa:", "", names(keggLink("pathway", "hsa"))),
-                   gsub("path:", "", keggLink("pathway", "hsa"))),
-      reactome = as.list(reactomePATHID2EXTID),
-      uniprot = load_keyword_genesets(9606)
+        "GO:BP" = "GO:BP",
+        "GO:MF" = "GO:MF",
+        KEGG = split(gsub("hsa:", "", names(keggLink("pathway","hsa"))),
+                     gsub("path:", "", keggLink("pathway", "hsa"))),
+        reactome = as.list(reactomePATHID2EXTID),
+        uniprot = load_keyword_genesets(9606),
+        "msigdb:C7:IMMUNESIGDB" = "msigdb:C7:IMMUNESIGDB"
     )
-    
+
+    background_df <- load_vmr_background(tissue)
     for (gs in names(gene_sets)) {
       message("Running GREAT for ", gs)
-      
       res <- great(vmr, gene_sets[[gs]], "RefSeq:hg38",
-                   background = paste0("chr", 1:22))
+                   background = background_df)
       tb  <- getEnrichmentTable(res)
+      new_gs <- gsub(":", "_", gs)
+      outfile <- paste0(tissue, "_", filter_label, "_", new_gs, ".csv")
       write.csv(
         tb,
-        file = here("heritability/gcta/tissue_comparison/functional_enrichment/_m",
-                    paste0(tissue, "_", filter_label, "_", gs, ".csv")),
+        file = here("heritability/gcta/tissue_comparison",
+                    "functional_enrichment/_m", outfile),
         row.names = FALSE
       )
     }
@@ -112,14 +127,14 @@ filter_grid <- expand.grid(
 purrr::pwalk(filter_grid, function(hfilter, pval, h2, label) {
   for (tissue in tissues) {
     message("Running enrichment: ", tissue, " - ", label)
-  
+
     # Apply the heritability and p-value filter
     vmr_filtered <- filter_heritability(tissue, hfilter, pval, h2)
     # Get enrichment for remaining data after filtering
     if (nrow(vmr_filtered) > 0) {
       get_enrichment(vmr_filtered, tissue, label)
     } else {
-      message("No data left after filtering for ", tissue, " - ", label)
+      message("No data left after filtering for ", tissue, " - ",label)
     }
   }
 })
