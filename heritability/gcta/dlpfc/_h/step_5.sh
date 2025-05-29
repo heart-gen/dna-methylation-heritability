224d30d1705de4f85b7ba05cf6ded1fd3c601bcd
Alexis Bennett <alexis.bennett@northwestern.edu>
2025-05-29 11:15:35 -0500

    Update number of array jobs for dlpfc and hippo

--- heritability/gcta/dlpfc/_h/step_5.sh
+++ heritability/gcta/dlpfc/_h/step_5.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks-per-node=1     # Number of cores (CPU)
 #SBATCH --mem=10G               # Memory limit
 #SBATCH --mail-type=FAIL
-#SBATCH --array=1-12078%250
+#SBATCH --array=1-10613%250
 #SBATCH --mail-user=alexis.bennett@northwestern.edu
 #SBATCH --job-name=greml     # Job name
 #SBATCH --output=/dev/null      # Standard output log
