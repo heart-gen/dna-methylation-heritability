5709037eebb9e640a8368d329bd038a031077ebc
KrotosBenjamin <kj.benjamin90@gmail.com>
2025-06-05 09:39:46 -0500

    adding scripts from real data for simulations

---
+++ simulation-analysis/elastic-net/sim_100n/_h/elastic_h2_array-01.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#SBATCH --account=p32505
+#SBATCH --partition=short
+#SBATCH --job-name=elastic_h2
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=kynon.benjamin@northwestern.edu
+#SBATCH --output=logs/elastic_h2_%A_%a.log
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=02:00:00
+
+log_message() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
+}
+
+log_message "**** Job starts ****"
+
+OFFSET=${OFFSET:-0} # fallback default to 0
+task_id=$((OFFSET + SLURM_ARRAY_TASK_ID - 1))
+export task_id
+
+echo "**** Quest info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOBID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURM_NODENAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "OFFSET: ${OFFSET}"
+echo "SLURM_ARRAY_TASK_ID: ${SLURM_ARRAY_TASK_ID}"
+echo "Computed task_id: ${task_id}"
+
+module purge
+module list
+
+ENV_PATH="/projects/p32505/opt/env"
+export region="caudate"
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export MKL_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+log_message "**** Run elastic net ****"
+conda run -p "${ENV_PATH}/r_env" Rscript ../_h/01.elastic-net.R
+
+log_message "**** Job ends ****"
