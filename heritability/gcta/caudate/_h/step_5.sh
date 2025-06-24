126bd1e50201b56fb84ef79d2bc6b0822daffd31
Alexis Bennett <alexis.bennett@northwestern.edu>
2025-06-24 15:09:07 -0500

    Update submission scripts for naming consistency

--- heritability/gcta/caudate/_h/step_5.sh
+++ heritability/gcta/caudate/_h/step_5.sh
@@ -4,24 +4,22 @@
 #SBATCH --time=01:00:00         # Time limit hrs:min:sec
 #SBATCH --nodes=1               # Number of nodes
 #SBATCH --ntasks-per-node=1     # Number of cores (CPU)
-#SBATCH --mem=10G               # Memory limit
+#SBATCH --mem=16G               # Memory limit
 #SBATCH --mail-type=FAIL
 #SBATCH --array=1-12078%250
 #SBATCH --mail-user=alexis.bennett@northwestern.edu
-#SBATCH --job-name=greml     # Job name
+#SBATCH --job-name=extract_snp  # Job name
 #SBATCH --output=/dev/null      # Standard output log
 #SBATCH --error=/dev/null       # Standard error log
 
 ## Edit with your job command
 REGION_LIST="./vmr_list.txt"
+SAMPLE_LIST="./samples.txt"
 CHR_FILE="/projects/b1213/resources/genomes/human/gencode-v47/fasta/chromosome_sizes.txt"
-DATA="/projects/p32505/projects/dna-methylation-heritability/inputs/genotypes"
-WORKING="./"
-OUTPUT="./h2"
+DATA="/projects/p32505/users/alexis/projects/dna-methylation-heritability/inputs/genotypes"
+OUTPUT="./plink_format"
 
-ENV_PATH="/projects/p32505/opt/env"
-
-# Get the current sample name from the sample list
+# Get the current region name from the region list
 REGION=$(sed -n "${SLURM_ARRAY_TASK_ID}p" $REGION_LIST)
 CHR=$(echo "$REGION" | awk '{print $1}')
 START=$(echo "$REGION" | awk '{print $2}')
@@ -34,8 +32,8 @@ LOG_DIR="logs/chr_${CHR}"
 mkdir -p "$LOG_DIR"
 
 # Redirect output and error logs to chr-specific log files
-exec > >(tee -a "$LOG_DIR/greml_${SLURM_ARRAY_TASK_ID}_out.log")
-exec 2> >(tee -a "$LOG_DIR/greml_${SLURM_ARRAY_TASK_ID}_err.log" >&2)
+exec > >(tee -a "$LOG_DIR/extract_snp_${SLURM_ARRAY_TASK_ID}_out.log")
+exec 2> >(tee -a "$LOG_DIR/extract_snp_${SLURM_ARRAY_TASK_ID}_err.log" >&2)
 
 # Log function
 log_message() {
@@ -55,51 +53,45 @@ echo "Task id: ${SLURM_ARRAY_TASK_ID}"
 ## List current modules for reproducibility
 
 module purge
-module load gcta/1.94.0
+module load plink/2.0-alpha-3.3
 module list
 
-##### GREML-LDMS #####
-# Check if SNP data exists
-BFILE="$WORKING/plink_format/chr_${CHR}/TOPMed_LIBD.AA.${START}_${END}"
+# check chromosome size information
 
-if [ ! -f "${BFILE}.bed" ] || [ ! -f "${BFILE}.bim" ] || [ ! -f "${BFILE}.fam" ]; then
-    log_message "SNP files for region $CHR: $START-$END not found. Skipping."
-    exit 0
-fi
+WINDOW=500000
+CHR_SIZE=$(grep "^chr1[[:space:]]" $CHR_FILE | cut -f2)
 
-log_message "Performing GREML analysis on $CHR: $START-$END"
+START_POS=$((START - WINDOW))
+END_POS=$((END + WINDOW))
 
-# Calculate SNP LD scores
-gcta64 --bfile $WORKING/plink_format/chr_${CHR}/TOPMed_LIBD.AA.${START}_${END} \
-       --ld-score-region 200 \
-       --out $CHR_DIR/TOPMed_LIBD.AA.${START}_${END}
+echo "Extracting SNPs from all subjects on $CHR: $START-$END ($WINDOW bp window)"
 
-## Activate conda environment
-# Stratify SNPs based on individual LD scores
-conda run -p $ENV_PATH/r_env Rscript ../_h/05.stratify_LD.R $CHR $START $END
-
-if [ $? -ne 0 ]; then
-    log_message "Error: Conda or script execution failed"
+if (( START_POS <= 0 )); then
+    echo "ERROR: Start position is below zero."
     exit 1
 fi
 
-# Make GRM for each group
-for i in {1..4} ; do
-    gcta64 --bfile $WORKING/plink_format/chr_${CHR}/subset_TOPMed_LIBD.AA.${START}_${END} \
-           --extract $CHR_DIR/${START}_${END}_snp_group_${i}.txt \
-           --make-grm \
-           --out $CHR_DIR/TOPMed_LIBD.AA.${START}_${END}_group_${i}
+if (( END_POS >= CHR_SIZE )); then
+    echo "ERROR: End position exceeds Chromosome $CHR size."
+    exit 1
+fi
 
-    echo "$CHR_DIR/TOPMed_LIBD.AA.${START}_${END}_group_${i}" >> \
-         $CHR_DIR/${START}_${END}_multi_GRMs.txt
-done
+plink2 --pfile "$DATA/TOPMed_LIBD.AA" \
+       --chr "$CHR" \
+       --from-bp "$START_POS" \
+       --to-bp "$END_POS" \
+       --make-bed \
+       --out "$CHR_DIR/TOPMed_LIBD.AA.${START}_${END}"
 
-# GREML with multiple GRM
-gcta64 --reml \
-       --mgrm $CHR_DIR/${START}_${END}_multi_GRMs.txt \
-       --pheno $WORKING/vmr/chr_${CHR}/${START}_${END}_meth.phen \
-       --covar $WORKING/covs/chr_${CHR}/TOPMed_LIBD.AA.covar \
-       --qcovar $WORKING/covs/chr_${CHR}/TOPMed_LIBD.AA.qcovar \
-       --out $CHR_DIR/TOPMed_LIBD.AA.${START}_${END}
+echo "Extracting SNPs from AA subjects on $CHR: $START-$END ($WINDOW bp window)" 
+
+# Subset of SNPs in AA cohort
+plink2 --pfile "$DATA/TOPMed_LIBD.AA" \
+       --chr "$CHR" \
+       --from-bp "$START_POS" \
+       --to-bp "$END_POS" \
+       --keep "$SAMPLE_LIST" \
+       --make-bed \
+       --out "$CHR_DIR/subset_TOPMed_LIBD.AA.${START}_${END}"
 
 log_message "**** Job ends ****"
