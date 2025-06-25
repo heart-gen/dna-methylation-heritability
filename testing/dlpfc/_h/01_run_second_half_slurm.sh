e91a599b475798a6ff1b38bb8ce867dc57fcff87
Elisa Johnson <elisajohnson2027@u.northwestern.edu>
2025-06-25 09:49:38 -0500

    Installs pipeline

---
+++ testing/dlpfc/_h/01_run_second_half_slurm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --output=run_second_half_slurm.log
+#SBATCH --mem=25G
+
+#  After running 'install_software.sh', this should point to the directory
+#  where this repo was cloned, and not say "$PWD"
+ORIG_DIR=/home/uyb5533/BiocMAP
+
+export _JAVA_OPTIONS="-Xms8g -Xmx10g"
+
+$ORIG_DIR/Software/bin/nextflow $ORIG_DIR/second_half.nf \
+    --annotation "$ORIG_DIR/ref" \
+    --sample "paired" \
+    --reference "hg38" \
+    -profile second_half_slurm
