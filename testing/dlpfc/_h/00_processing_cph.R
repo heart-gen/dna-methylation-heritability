fe4964e94e01a60de0dbf6629d8f8e83e2cc07f5
Elisa Johnson <elisajohnson2027@u.northwestern.edu>
2025-06-25 09:50:40 -0500

    Deletes unnecessary files

--- testing/dlpfc/_h/00_processing_cph.R
+++
@@ -1,9 +0,0 @@
-## Load the package at the top of your script
-library("sessioninfo")
-
-## Reproducibility information
-print('Reproducibility information:')
-Sys.time()
-proc.time()
-options(width = 120)
-session_info()
