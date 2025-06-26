b18f29c97b431abc2b44a2ccd7f46b2e61a98dd8
Alexis Bennett <alexis.bennett@northwestern.edu>
2025-06-26 10:58:10 -0500

    Split into two scripts for clarity

--- heritability/gcta/caudate/_h/02.pca.R
+++ heritability/gcta/caudate/_h/02.pca.R
@@ -58,6 +58,7 @@ pca <- function(res, output_path) {
 
                                         # write file
     write.csv(pc$x, file = file.path(output_path, "pc.csv"))
+    
     return(pc)
 }
 
@@ -72,50 +73,6 @@ corr_pc_ances <- function(pc, gen_ances, idx, output_path) {
     write.csv(res, file = file.path(output_path, "pc_ances_cor.csv"))
 }
 
-remove_ct_snps <- function(snp_file_path, meth, pos) {
-    snp <- fread(snp_file_path, header=F, data.table=F)
-    idx <- is.element(pos[,1], snp[,1])
-    meth <- meth[,!idx]
-    pos <- pos[!idx,1]
-    return(list(meth, pos))
-}
-
-filter_pheno <- function(brnum, meth, pc, ances_file_path, pheno_file_path) {
-  ances <- read.table(ances_file_path, header=TRUE)
-  demo  <- read.csv(pheno_file_path, header = TRUE)
-  samples <- intersect(intersect(ances$brnum[ances$group == "AA"],brnum),
-                       demo$BrNum[demo$Region == "Caudate" & demo$Age >= 17])
-  meth <- meth[match(samples,brnum),]
-  
-  # align samples
-  ind <- intersect(samples, pc$X)
-  pc <- pc[match(ind, pc$X), -1]
-  meth <- meth[match(ind, samples), ]
-  return(list(meth, pc))
-}
-
-regress_pcs <- function(meth, pc, chr, pos, output_path){
-  residuals <- meth
-  for(i in 1:ncol(meth)){
-    if(! i %% 100){
-      cat(i,"\n")
-    }
-    d <- as.data.frame(cbind(y=meth[,i],pc[,1:5]))
-    model = lm(y ~ .,data=d)
-    residuals[,i] = resid(model)
-  }
-  
-  # get variance for residual
-  res_var <- apply(residuals,2,sd)
-  
-  # output
-  f_out <- file.path(output_path, "res_var.tsv")
-  out <- data.frame(chr=chr, pos=pos, sd=res_var)
-  write.table(out, f_out, col.names=F, row.names=F, sep="\t", quote=F)
-  return(out)
-}
-
-
 ## Main
                                         # create output directories if they  
                                         # don't exist
@@ -128,19 +85,7 @@ if (!dir.exists(output_path)) {
 load(here("heritability", "gcta", "caudate", "_m", "cpg", paste0("chr_", chr), "stats.rda"))
 v <- data.frame(chr = chr, start = start(BSobj), sd = sds)
 
-# set file paths
-cpg_meth <- here("heritability", "gcta", "caudate", "_m", "cpg", paste0("chr_", chr), "cpg_meth.phen")
-cpg_names <- here("heritability", "gcta", "caudate", "_m", "cpg", paste0("chr_", chr), "cpg_pos.txt")
-pheno_file_path <- here("inputs/phenotypes/merged/_m/merged_phenotypes.csv")
-snp_file_path <- paste0("/projects/b1213/resources/libd_data/wgbs/DEM2/snps_CT/chr",chr)
-
-# read data
-meth <- fread(cpg_meth, header=TRUE, data.table=FALSE)
-brnum <- meth[, 1]
-meth <- meth[,-c(1, 2)]
-pos <- read.table(cpg_names, header=FALSE)
-pos <- pos[-c(1, 2), , drop = FALSE]
-
+                                        # get top 1M variable CpG
 meth_top <- get_top_meth(BSobj, v)
 
                                         # load genetic ancestry
@@ -158,12 +103,6 @@ pc <- pca(res, output_path)
 
                                         # correlation of pc with ancestry
 corr_pc_ances(pc, ances$gen_ances, ances$idx, output_path)
-
-filtered_cpg <- remove_ct_snps(snp_file_path, meth, pos)
-
-filtered_samples <- filter_pheno(brnum, filtered_cpg$meth, pc, ances_file_path, pheno_file_path)
-
-out <- regress_pcs(filtered_samples$meth, filtered_samples$pc, chr, filtered_cpg$pos, output_path)
 
 #### Reproducibility information ####
 print("Reproducibility information:")
