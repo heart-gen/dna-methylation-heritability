f76fac70551280448a0049db3625f1ed8d97b3fc
Elisa Johnson <elisajohnson2027@u.northwestern.edu>
2025-06-27 14:00:14 -0500

    Adds reproducibility info and SBATCH

--- testing/dlpfc/_h/01_run_second_half_slurm.sh
+++ testing/dlpfc/_h/01_run_second_half_slurm.sh
@@ -1,9 +1,41 @@
 #!/bin/bash
-#SBATCH --output=run_second_half_slurm.log
-#SBATCH --mem=25G
 
 #  After running 'install_software.sh', this should point to the directory
 #  where this repo was cloned, and not say "$PWD"
+
+#SBATCH --account=b1042        # Replace with your allocation
+#SBATCH --partition=short       # Partition (queue) name
+#SBATCH --time=01:00:00         # Time limit hrs:min:sec
+#SBATCH --nodes=1               # Number of nodes
+#SBATCH --ntasks-per-node=1     # Number of cores (CPU)
+#SBATCH --mem=1G                # Memory limit
+#SBATCH --job-name=example_job  # Job name
+#SBATCH --output=output_%j.log  # Standard output log
+#SBATCH --error=error_%j.log    # Standard error log
+
+# Log function
+log_message() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
+}
+
+log_message "**** Job starts ****"
+
+echo "**** QUEST info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOBID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURM_NODENAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID:-N/A}"
+
+## List current modules for reproducibility
+
+module purge
+module load fastqc/0.12.0
+module list
+
+# Your commands go here
+
 ORIG_DIR=$PWD
 
 export _JAVA_OPTIONS="-Xms8g -Xmx10g"
