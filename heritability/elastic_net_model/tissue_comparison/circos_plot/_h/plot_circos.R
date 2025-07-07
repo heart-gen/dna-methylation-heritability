38e1ded6811567b61721d4c842c60fc575407837
Alexis Bennett <alexis.bennett@northwestern.edu>
2025-07-07 13:05:08 -0500

    Adapt script for VMR analysis

--- heritability/elastic_net_model/tissue_comparison/circos_plot/_h/plot_circos.R
+++ heritability/elastic_net_model/tissue_comparison/circos_plot/_h/plot_circos.R
@@ -1,37 +1,35 @@
 ## Generate circlized plot
 suppressPackageStartupMessages({
     library(dplyr)
-    library(biomaRt)
+    library(here)
     library(circlize)
     library(ComplexHeatmap)
 })
 
-get_annotation <- function(){
-    ensembl = useEnsembl(biomart="ensembl", dataset="hsapiens_gene_ensembl")
-    biomart = getBM(attributes=c('ensembl_gene_id', 'chromosome_name',
-                                 'start_position', 'end_position'),
-                    mart=ensembl)
-    return(biomart)
-}
-memMART <- memoise::memoise(get_annotation)
-
 extract_bed <- function(tissue){
-    fn = "../../summary_table/_m/BrainSeq_ancestry_4features_4regions.txt.gz"
-    bed = data.table::fread(fn) %>% filter(Type == "Gene", Tissue == tissue) %>%
-        mutate(ensembl_gene_id=gsub("\\..*", "", gencodeID)) %>%
-        inner_join(memMART(), by=c("ensembl_gene_id")) %>%
-        dplyr::select(chromosome_name,start_position,
-                      end_position,posterior_mean,lfsr) %>%
-        mutate(chromosome_name=paste0('chr', chromosome_name))
-    bed_EA = bed %>% filter(posterior_mean > 0)
-    bed_AA = bed %>% filter(posterior_mean < 0)
-    return(list("EA"=bed_EA, "AA"=bed_AA))
+    enet_file <- here("heritability/elastic_net_model/", 
+                      paste0(tissue, "/_m/", tissue, "_summary_elastic-net.tsv"))
+    vmr <- data.table::fread(enet_file) %>%
+      dplyr::select(chrom, start, end, h2_unscaled, r_squared_cv) %>%
+      mutate(chrom = paste0("chr", chrom)) %>%
+      rename(chr = chrom)
+    
+    vmr <- vmr %>%
+      mutate(h2_category = case_when(
+        r_squared_cv <= 0.75 ~ "Low prediction",
+        h2_unscaled < 0.1 & r_squared_cv > 0.75 ~ "Non-heritable",
+        h2_unscaled >= 0.1 & r_squared_cv > 0.75 ~ "Heritable"
+      ))
+    
+    vmr_groups <- split(vmr, vmr$h2_category)
+    
+    return(vmr_groups)
 }
 
-plot_circos_4tissue <- function(caudate, dlpfc, hippo, gyrus){
-    lgd_points = Legend(at=c("Increased EA proportion", "Increased AA proportion"),
-                        type="points", legend_gp=gpar(col = c("red", "blue")),
-                        title_position="topleft", title="Genetic Ancestry",
+plot_circos_3tissue <- function(caudate, dlpfc, hippo){
+    lgd_points = Legend(at=c("Heritable", "Non-heritable", "Low prediction"),
+                        type="points", legend_gp=gpar(col = c("red", "blue", "green")),
+                        title_position="topleft", title="Heritability",
                         background="#FFFFFF")
     circos.clear() # clear plot if there is any
     circos.par("start.degree" = 0, "cell.padding" = c(0, 0, 0, 0),
@@ -44,42 +42,34 @@ plot_circos_4tissue <- function(caudate, dlpfc, hippo, gyrus){
                         panel.fun = function(region, value, ...) {
                             i = getI(...)
                             circos.genomicPoints(region, value, pch = 16,
-                                                 cex = 0.6, col = c("red", "blue")[i], ...)
-    })
-    circos.genomicTrack(gyrus, bg.border="#4DBBD5FF",
-                        bg.col=add_transparency("#4DBBD5FF", transparency=0.7),
-                        panel.fun = function(region, value, ...) {
-                            i = getI(...)
-                            circos.genomicPoints(region, value, pch = 16,
-                                                 cex = 0.6, col = c("red", "blue")[i], ...)
+                                                 cex = 0.6, col = c("red", "blue", "green")[i], ...)
     })
     circos.genomicTrack(dlpfc, bg.border="#00A087FF",
                         bg.col=add_transparency("#00A087FF", transparency=0.7),
                         panel.fun = function(region, value, ...) {
                             i = getI(...)
                             circos.genomicPoints(region, value, pch = 16,
-                                                 cex = 0.6, col = c("red", "blue")[i], ...)
+                                                 cex = 0.6, col = c("red", "blue", "green")[i], ...)
     })
     circos.genomicTrack(hippo, bg.border="#3C5488FF",
                         bg.col=add_transparency("#3C5488FF", transparency=0.7),
                         panel.fun = function(region, value, ...) {
                             i = getI(...)
                             circos.genomicPoints(region, value, pch = 16,
-                                                 cex = 0.6, col = c("red", "blue")[i], ...)
+                                                 cex = 0.6, col = c("red", "blue", "green")[i], ...)
     })
     draw(lgd_points, x=unit(5, "mm"), y=unit(5, "mm"), just=c("left", "bottom"))
 }
 
 ####### MAIN
 main <- function(){
-    caudate <- extract_bed("Caudate")
-    gyrus   <- extract_bed("Dentate Gyrus")
-    dlpfc   <- extract_bed("DLPFC")
-    hippo   <- extract_bed("Hippocampus")
+    caudate <- extract_bed("caudate")
+    dlpfc   <- extract_bed("dlpfc")
+    hippo   <- extract_bed("hippocampus")
                                         # plot
-    pdf(file = paste0("significant_circos_plot_4regions.pdf"),
+    pdf(file = paste0("significant_circos_plot_3regions.pdf"),
         width = 10, height = 10)
-    plot_circos_4tissue(caudate, dlpfc, hippo, gyrus)
+    plot_circos_3tissue(caudate, dlpfc, hippo)
     dev.off()
 }
 
