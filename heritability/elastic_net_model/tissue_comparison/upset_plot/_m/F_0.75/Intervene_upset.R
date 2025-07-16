8f0c45ab002a9ac8050fa22f71989cb5ce80fe6f
Alexis Bennett <alexis.bennett@northwestern.edu>
2025-07-16 12:16:25 -0500

    Add script and initial upset plots

---
+++ heritability/elastic_net_model/tissue_comparison/upset_plot/_m/F_0.75/Intervene_upset.R
@@ -0,0 +1,7 @@
+#!/usr/bin/env Rscript
+if (suppressMessages(!require("UpSetR"))) suppressMessages(install.packages("UpSetR", repos="http://cran.us.r-project.org"))
+library("UpSetR")
+pdf("./F_0.75/Intervene_upset.pdf", width=14, height=8, onefile=FALSE, useDingbats=FALSE)
+expressionInput <- c('hippocampus'=4134,'dlpfc'=4585,'dlpfc&hippocampus'=4258,'caudate'=8367,'caudate&hippocampus'=791,'caudate&dlpfc'=657,'caudate&dlpfc&hippocampus'=1770)
+upset(fromExpression(expressionInput), nsets=3, nintersects=30, show.numbers="yes", main.bar.color="#ea5d4e", sets.bar.color="#317eab", empty.intersections=NULL, order.by = "freq", number.angles = 0, mainbar.y.label ="No. of Intersections", sets.x.label ="Set size")
+invisible(dev.off())
