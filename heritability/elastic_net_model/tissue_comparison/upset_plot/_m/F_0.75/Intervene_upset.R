21cb6f8f593e728596aa3d8b0a69227f48d21d0a
Alexis Bennett <alexis.bennett@northwestern.edu>
2025-07-16 12:46:36 -0500

    Testing plot colors

--- heritability/elastic_net_model/tissue_comparison/upset_plot/_m/F_0.75/Intervene_upset.R
+++ heritability/elastic_net_model/tissue_comparison/upset_plot/_m/F_0.75/Intervene_upset.R
@@ -3,5 +3,5 @@ if (suppressMessages(!require("UpSetR"))) suppressMessages(install.packages("UpS
 library("UpSetR")
 pdf("./F_0.75/Intervene_upset.pdf", width=14, height=8, onefile=FALSE, useDingbats=FALSE)
 expressionInput <- c('hippocampus'=4134,'dlpfc'=4585,'dlpfc&hippocampus'=4258,'caudate'=8367,'caudate&hippocampus'=791,'caudate&dlpfc'=657,'caudate&dlpfc&hippocampus'=1770)
-upset(fromExpression(expressionInput), nsets=3, nintersects=30, show.numbers="yes", main.bar.color="#ea5d4e", sets.bar.color="#317eab", empty.intersections=NULL, order.by = "freq", number.angles = 0, mainbar.y.label ="No. of Intersections", sets.x.label ="Set size")
+upset(fromExpression(expressionInput), nsets=3, nintersects=30, show.numbers="yes", main.bar.color="#36383B", sets.bar.color="#36383B", empty.intersections=NULL, order.by = "freq", number.angles = 0, mainbar.y.label ="No. of Intersections", sets.x.label ="Set size")
 invisible(dev.off())
