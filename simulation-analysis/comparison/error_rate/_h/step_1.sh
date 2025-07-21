19dabc12b1b8e95f52a522984dad48b46a6ac3cd
Alexis Bennett <alexis.bennett@northwestern.edu>
2025-07-21 15:06:19 -0500

    Add submission script and results

---
+++ simulation-analysis/comparison/error_rate/_h/step_1.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#SBATCH --account=p32505        # Replace with your allocation
+#SBATCH --partition=short       # Partition (queue) name
+#SBATCH --time=00:10:00         # Time limit hrs:min:sec
+#SBATCH --nodes=1               # Number of nodes
+#SBATCH --ntasks-per-node=1     # Number of cores (CPU)
+#SBATCH --mem=8G                # Memory limit
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=alexis.bennett@northwestern.edu
+#SBATCH --job-name=error_rate # Job name
+#SBATCH --output=logs/error_rate.%j.log # Standard output log
+
+# Log function
+log_message() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
+}
+
+log_message "**** Job starts ****"
+
+echo "**** QUEST info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOBID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURM_NODENAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID:-N/A}"
+
+## List current modules for reproducibility
+
+module purge
+module list
+
+# Set path variables
+ENV_PATH="/projects/p32505/opt/env"
+
+log_message "Calculating empirical power and type 1 error rate"
+
+## Activate conda environment
+conda run -p $ENV_PATH/AI_env python ../_h/01.error_rate.py
+
+if [ $? -ne 0 ]; then
+    log_message "Error: Conda or script execution failed"
+    exit 1
+fi
+
+log_message "**** Job ends ****"
