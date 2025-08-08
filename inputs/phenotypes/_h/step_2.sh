fc0f8998c401ad83f18eb86c1d46e8eb9e83aa40
KrotosBenjamin <kj.benjamin90@gmail.com>
2025-08-08 11:41:42 -0500

    changed partition

--- inputs/phenotypes/_h/step_2.sh
+++ inputs/phenotypes/_h/step_2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --account=p32505
-#SBATCH --partition=short
+#SBATCH --account=b1042
+#SBATCH --partition=genomics
 #SBATCH --job-name=gen_phenotype
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=kynon.benjamin@northwestern.edu
