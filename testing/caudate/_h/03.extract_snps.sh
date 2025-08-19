5120d89d84b54f54937a2b86f1f85d68b7ccc637
Alexis Bennett <alexis.bennett@northwestern.edu>
2025-08-19 14:03:24 -0500

    Update to utilize array jobs

--- testing/caudate/_h/03.extract_snps.sh
+++ testing/caudate/_h/03.extract_snps.sh
@@ -1,52 +1,62 @@
 #!/bin/bash
 #SBATCH --account=b1042
-#SBATCH --partition=genomics-gpu
-#SBATCH --time=20:00:00
+#SBATCH --partition=genomics
+#SBATCH --time=04:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --gres=gpu:a100:1
-#SBATCH --mem=40G
-#SBATCH --array=0-66
+#SBATCH --mem=12G
+#SBATCH --array=0-8118
 #SBATCH --job-name=extract_snps
-#SBATCH --output=logs/chr_21/extract_snps_out.log
-#SBATCH --error=logs/chr_21/extract_snps_err.log
+#SBATCH --output=logs/extract_snps_%A_%a.out
+#SBATCH --error=logs/extract_snps_%A_%a.err
 
 log_message() {
     echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
 }
 log_message "**** Job starts ****"
 
-# --- Static params ---
-CHR="21"
-REGION_DIR="./chunked_cpg/chr_${CHR}"
-CHROM_SIZES="/projects/b1213/resources/genomes/human/gencode-v47/fasta/chromosome_sizes.txt"
-BASE_NAME="/projects/b1213/users/alexis/projects/dna-methylation-heritability/inputs/genotypes/TOPMed_LIBD.AA"
-SAMPLE_LIST="./samples.txt"
-OUTPUT_DIR="plink_outputs/chr_${CHR}"
-mkdir -p "$OUTPUT_DIR"
+## List current modules for reproducibility
 
-# --- Load GNU parallel ---
-module load parallel/20160922
+module purge
+module load plink/2.0-alpha-3.3
+module list 
 
 # --- Derive chunk file ---
-CHUNK_FILE=$(printf "$REGION_DIR/chunk_%03d.bed" "$SLURM_ARRAY_TASK_ID")
+CHUNK_FILE=$(sed -n "$((SLURM_ARRAY_TASK_ID + 1))p" chunk_file_list.txt)
+
 if [[ ! -f "$CHUNK_FILE" ]]; then
     echo "Missing region file: $CHUNK_FILE"
     exit 1
 fi
 
-# --- Parallel PLINK extraction ---
-parallel -j ${SLURM_CPUS_PER_TASK} --colsep '\t' "
+# Extract CHR from path: cpg/chr_1/chunked_cpg/chunk_001.bed
+CHR=$(echo "$CHUNK_FILE" | sed -E 's|.*chr_([0-9XY]+).*|\1|')
+
+# --- Static params ---
+BASE_NAME="../../../inputs/genotypes/TOPMed_LIBD.AA"
+SAMPLE_LIST="../../../heritability/caudate/_m/samples.txt"
+OUTPUT_DIR="./plink_outputs/chr_${CHR}"
+mkdir -p "$OUTPUT_DIR"
+
+log_message "Processing chunk: $CHUNK_FILE"
+
+while IFS=$'\t' read -r chr start end label; do
+    OUT_FILE="${OUTPUT_DIR}/cpg_TOPMed_LIBD.AA.${start}_${end}"
+
+    log_message "Extracting SNPs for $chr:$start-$end → $OUT_FILE"
+
     plink2 \
-      --pfile $BASE_NAME \
-      --chr $CHR \
-      --extract range <(echo {1} '\t' {2} '\t' {3}) \
-      --make-bed \
-      --keep $SAMPLE_LIST \
-      --no-parents \
-      --no-sex \
-      --no-pheno \
-      --out $OUTPUT_DIR/chr${CHR}_region_{#}_snps"
+        --pfile "$BASE_NAME" \
+        --chr "$CHR" \
+        --from-bp "$start" \
+        --to-bp "$end" \
+        --make-bed \
+        --keep "$SAMPLE_LIST" \
+        --no-parents \
+        --no-sex \
+        --no-pheno \
+        --out "$OUT_FILE"
+done < "$CHUNK_FILE"
 
 log_message "**** Job ends ****"
