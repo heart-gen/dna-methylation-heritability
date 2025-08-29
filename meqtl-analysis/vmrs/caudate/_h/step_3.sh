c3b51dcd0846928a92c2d97e0d2f8ec3e5335e01
Alexis Bennett <alexis.bennett@northwestern.edu>
2025-08-29 14:53:56 -0500

    Update script for genotype preparation; This closes #12

--- meqtl-analysis/vmrs/caudate/_h/step_3.sh
+++ meqtl-analysis/vmrs/caudate/_h/step_3.sh
@@ -3,11 +3,11 @@
 #SBATCH --partition=short
 #SBATCH --job-name=preprocess_genotypes
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=sierramannion2028@u.northwestern.edu
+#SBATCH --mail-user=alexis.bennett@northwestern.edu
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=20gb
-#SBATCH --output=genotypes.%j.log
+#SBATCH --output=logs/genotypes.%j.log
 #SBATCH --time=00:30:00
 
 # Function to echo with timestamp
@@ -29,18 +29,23 @@ echo "Task id: ${SLURM_ARRAY_TASK_ID:-N/A}"
 log_message "**** Loading modules ****"
 
 module purge
+module load plink/2.0-alpha-3.3
 module list
 
 plink2 --version
 ## Edit with your job command
-OUTDIR="protected_data"
-GENOTYPES="/projects/b1213/large_projects/localQTL-benchmarking/inputs/genotypes"
+OUTDIR="./protected_data"
+GENOTYPES="../../../../inputs/genotypes"
+SAMPLES="../../../../heritability/caudate/_m/samples.txt"
 
 log_message "**** Format genotypes ****"
 mkdir -p $OUTDIR
 
-plink2 --pfile $GENOTYPES/_m/TOPMed_LIBD \
-       --keep keepPsam.txt --make-pgen \
+plink2 --pfile $GENOTYPES/TOPMed_LIBD \
+       --keep $SAMPLES --make-pgen \
+       --no-parents \
+       --no-sex \
+       --no-pheno \
        --out $OUTDIR/TOPMed_LIBD
 
 log_message "**** Job ends ****"
