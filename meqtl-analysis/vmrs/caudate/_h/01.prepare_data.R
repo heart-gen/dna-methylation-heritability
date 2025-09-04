165081d719280ff78585911ebd8f906524a02d1f
Alexis Bennett <alexis.bennett@northwestern.edu>
2025-09-04 13:42:21 -0500

    Combines 01.prepare_data and 02.preprocess_data for VMR analysis; closes #10 and closes #11

--- meqtl-analysis/vmrs/caudate/_h/01.prepare_data.R
+++ meqtl-analysis/vmrs/caudate/_h/01.prepare_data.R
@@ -1,60 +1,98 @@
-## Script to prepare the data
+## Script to prepare inputs for meQTL analysis of VMRs
 suppressMessages({
     library(here)
     library(dplyr)
     library(SummarizedExperiment)
+    library(data.table)
 })
 
-#### Phenotype data
-                                        # Download from eQTL browser
-caud8_file <- here("inputs/counts/_m",
-                   "rse-gene.bsp3.caudate-n487.gencode-v47.RData")
-load(caud8_file)
-                                        # Select variables of interest
-                                        # Update after confounding analysis
-fields   <- c('BrNum', 'RNum', 'Region', 'RIN', 'Age', 'Sex', 'Race',
-              'Dx', 'MoD', "dropped", "mito_mapping_rate", 'mapped_percent')
-pheno    <- colData(rse)[,fields]
-                                        # Basic filtering
-pheno <- filter(as.data.frame(pheno), Age > 13, dropped == "f",
-                Race %in% c("AA", "CAUC"))
-                                        # Save file
-as.data.frame(pheno) |>
-    write.csv(file = "phenotypes.csv")
+#### Main
 
-#### Normalized counts
-keepIndex <- which(rse$Age > 13 & (rse$dropped == "f") &
-                   rse$Race %in% c("AA", "CAUC"))
-rse  <- rse[, keepIndex]
-rm(keepIndex)
-                                        # Generate DGE list
-x      <- edgeR::DGEList(counts=assays(rse)$counts[, pheno$RNum],
-                         genes=rowData(rse), samples=pheno)
-                                        # Filter by expression
-design <- model.matrix(~Dx, data=x$samples)
-keep.x <- edgeR::filterByExpr(x, design=design)
-print(paste('There are:', sum(keep.x), 'features left!', sep=' '))
-x      <- x[keep.x, , keep.lib.sizes=FALSE]
-                                        # Normalize library size
-x      <- edgeR::calcNormFactors(x, method="TMM")
-                                        # Normalize counts
-cpm    <- edgeR::cpm(x)
-                                        # Save data
-as.data.frame(cpm) |> tibble::rownames_to_column("feature_id") |>
-    write.table(file=gzfile("normalized_expression.txt.gz"),
-                sep="\t", quote=FALSE, row.names=FALSE)
-rm(keep.x, design, cpm)
+                                        # Create output dir
+output_path <- here("meqtl-analysis", "vmrs", "caudate", "_m")
+
+if (!dir.exists(output_path)) {
+  dir.create(output_path, recursive = TRUE)
+}
+
+
+#### Phenotype data
+
+                                        # Read in pheno data
+pheno_file_path <- here("inputs/phenotypes/_m/phenotypes-AA.tsv")
+pheno <- fread(pheno_file_path, header = TRUE)
+
+sample_file_path <- here("heritability/caudate/_m/samples.txt")
+samples <- fread(sample_file_path, header = FALSE)
+colnames(samples) <- c("brnum", "fid")
+
+                                        # Basic filtering
+pheno <- pheno |>
+        select(brnum, agedeath, sex, race, primarydx, region) |>
+        filter(race == "AA", agedeath >= 17, region == "caudate") |>
+        inner_join(samples, by = "brnum")
+
+                                        # Save filtered phenotype file
+out_pheno   <- file.path(output_path, "phenotypes.csv")
+as.data.frame(pheno) |>
+    write.csv(file = out_pheno)
+
+#### Combine residualized methylation matrix
+
+                                        # Read in .phen files
+meth_file_path <- here("heritability/caudate/_m_to_del/vmr")
+meth_files <- list.files(path = meth_file_path, pattern = "_meth\\.phen$", 
+                         recursive = TRUE, full.names = TRUE)
+
+feature_ids <- paste0("VMR", seq_along(meth_files))
+meth_list <- vector("list", length(meth_files))
+for (i in seq_along(meth_files)) {
+  df <- fread(meth_files[i], select = c("V1", "V3"))
+  colnames(df) <- c("brnum", "meth")
+  meth_list[[i]] <- df
+}
+
+                                        # Add brnum and feature IDs
+brnums <- unique(unlist(lapply(meth_list, function(df) df$brnum)))
+meth_matrix <- matrix(NA, nrow = length(meth_files), ncol = length(brnums),
+                   dimnames = list(feature_ids, brnums))
+
+for (i in seq_along(meth_list)) {
+  df <- meth_list[[i]]
+  meth_matrix[i, df$brnum] <- df$meth
+}
+
+                                        # Save meth matrix
+out_meth   <- file.path(output_path, "normalized_methylation.tsv")
+as.data.frame(meth_matrix) |> tibble::rownames_to_column("feature_id") |>
+    write.table(file=out_meth, sep="\t", 
+                quote=FALSE, row.names=FALSE)
+
+#data.table::fwrite(meth_df, "norm_vmr.tsv", sep='\t', row.names=TRUE)
 
 #### Export annotation
-genes_to_keep <- rownames(x$genes)
+bed_file_path <- here("heritability/caudate/_m_to_del/vmr.bed")
+vmr_bed <- fread(bed_file_path, header = FALSE)
+colnames(vmr_bed) <- c("seqnames", "start", "end")
 
-annot <- rowRanges(rse) |> as.data.frame() |> distinct() |>
-    tibble::rownames_to_column("feature_id") |>
-    filter(gene_id %in% genes_to_keep) |>
-    select(seqnames, start, end, feature_id, strand) |>
-    mutate(index=feature_id) |> tibble::column_to_rownames("index")
-                                        # Save data
-data.table::fwrite(annot, "feature.bed", sep='\t', row.names=TRUE)
+                                        # Format bed file
+annot <- tibble::rowid_to_column(vmr_bed, "feature_id") |> 
+  mutate(seqnames = paste0("chr", seqnames),
+         feature_id = paste0("VMR", row_number())) |>
+  select(seqnames, start, end, feature_id) |> 
+  mutate(index=feature_id) |> 
+  tibble::column_to_rownames("index")
+
+                                        # Save feature bed file
+out_vmr <- file.path(output_path, "feature.bed")
+data.table::fwrite(annot, out_vmr, sep='\t', row.names=TRUE)
+
+#### Export chr list
+out_chr <- file.path(output_path, "vcf_chr_list.txt")
+chr_list <- paste0("chr", c(1:22))
+write.table(chr_list, file = out_chr, quote = FALSE, 
+            row.names = FALSE, col.names = FALSE)
+
 
 #### R session information
 Sys.time()
