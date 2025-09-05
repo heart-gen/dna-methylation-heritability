3275ea0262fae713d56148e82dd5f7b730db311e
Alexis Bennett <alexis.bennett@northwestern.edu>
2025-09-05 16:45:34 -0500

    update expression script for vmrs

--- meqtl-analysis/vmrs/caudate/_h/04.prepare_expression.py
+++ meqtl-analysis/vmrs/caudate/_h/04.prepare_expression.py
@@ -3,94 +3,65 @@
 ## Edited by Kynon J Benjamin (05/05/2021)
 
 import argparse
-import gzip, os
+import os
 import subprocess
 import numpy as np
 import pandas as pd
-import scipy.stats as stats
 
 
-def read_gct(gct_file, sample_ids=None, dtype=None):
+def read_vmr(vmr_file, sample_ids=None):
     """
-    Load GCT as DataFrame. The first two columns must be 'Name' and 'Description'.
+    Load VMR methylation matrix (feature_id x samples).
+    Assumes first column = feature_id, remaining columns = samples.
     """
+    df = pd.read_csv(vmr_file, sep='\t', index_col=0)
     if sample_ids is not None:
-        sample_ids = ['Name']+list(sample_ids)
-    if gct_file.endswith('.gct.gz') or gct_file.endswith('.gct'):
-        if dtype is not None:
-            with gzip.open(gct_file, 'rt') as gct:
-                gct.readline()
-                gct.readline()
-                sample_ids = gct.readline().strip().split()
-            dtypes = {i:dtype for i in sample_ids[2:]}
-            dtypes['Name'] = str
-            dtypes['Description'] = str
-            df = pd.read_csv(gct_file, sep='\t', skiprows=2, usecols=sample_ids,
-                             index_col=0, dtype=dtypes)
-        else:
-            df = pd.read_csv(gct_file, sep='\t', skiprows=2, usecols=sample_ids,
-                             index_col=0)
-    elif gct_file.endswith('.parquet'):
-        df = pd.read_parquet(gct_file, columns=sample_ids)
-    elif gct_file.endswith('.ft'):  # feather format
-        df = pd.read_feather(gct_file, columns=sample_ids)
-        df = df.set_index('Name')
-    else:
-        raise ValueError('Unsupported input format.')
+        sample_ids = [s for s in sample_ids if s in df.columns]
+        df = df.loc[:, sample_ids]
     df.index.name = 'feature_id'
-    if 'Description' in df.columns:
-        df = df.drop('Description', axis=1)
     return df
 
 
-def sort_samples(norm_df, vcf_lookup_s, sort=True):
+def sort_samples(vmr_df, vcf_lookup_s, sort=True):
     """
-    vcf_lookup: lookup table mapping sample IDs to VCF IDs
+    Ensure VMR matrix columns match VCF sample IDs.
+    vcf_lookup_s: pandas.Series mapping VMR sample IDs -> VCF IDs
     """
     if sort:
-        ix = np.intersect1d(norm_df.columns, vcf_lookup_s.index)
-        norm_df = norm_df[ix]
-    return norm_df
+        ix = np.intersect1d(vmr_df.columns, vcf_lookup_s.index)
+        vmr_df = vmr_df[ix]
+    return vmr_df
 
-
-def get_bed(bed_file, FLIP):
-    bed_template_df = pd.read_csv(bed_file, sep='\t', index_col=0)\
-                        .rename(columns={'seqnames':'chr'})\
-                        .loc[:, ["chr", "start", "end", "feature_id", "strand"]]
-    if FLIP:
-        # fix strand TSS assignment
-        gene_order = bed_template_df.index
-        bed_df_minus = bed_template_df[(bed_template_df["strand"] == "-")].copy()
-        bed_df_minus.loc[:, "start"] = bed_df_minus.end
-        bed_df_minus.loc[:, "end"] = bed_df_minus.end + 1
-        bed_df_plus = bed_template_df[(bed_template_df["strand"] == "+")].copy()
-        bed_df_plus.loc[:, "end"] = bed_df_plus.start + 1
-        bed_template_df = pd.concat([bed_df_plus, bed_df_minus], axis=0)\
-                            .drop(["strand"], axis=1)\
-                            .loc[gene_order, :]
-    else:
-        bed_template_df.loc[:, "end"] = bed_template_df.start + 1
+def get_bed(bed_file):
+    """
+    Load VMR BED annotation file.
+    Expected columns: seqnames, start, end, feature_id
+    """
+    bed_template_df = (
+        pd.read_csv(bed_file, sep='\t', index_col=0)
+          .rename(columns={'seqnames': 'chr'})
+          .loc[:, ["chr", "start", "end", "feature_id"]]
+    )
     return bed_template_df
-
 
 def prepare_bed(df, bed_template_df, chr_subset=None):
     bed_df = pd.merge(bed_template_df, df, left_on="feature_id",
                       right_index=True)
-    # sort by start position
+    # sort by start position within each chromosome
     bed_df = bed_df.groupby('chr', sort=False, group_keys=False)\
                    .apply(lambda x: x.sort_values('start'),
                           include_groups=True)
     if chr_subset is not None:
-        # subset chrs from VCF
         bed_df = bed_df[bed_df.chr.isin(chr_subset)]
     return bed_df
 
-
 def write_bed(bed_df, output_name):
     """
-    Write DataFrame to BED format
+    Write DataFrame to BED format (bgzipped + tabix indexed).
     """
-    assert bed_df.columns[0]=='chr' and bed_df.columns[1]=='start' and bed_df.columns[2]=='end'
+    assert bed_df.columns[0] == 'chr'
+    assert bed_df.columns[1] == 'start'
+    assert bed_df.columns[2] == 'end'
     # header must be commented in BED format
     header = bed_df.columns.values.copy()
     header[0] = '#chr'
@@ -100,53 +71,59 @@ def write_bed(bed_df, output_name):
     subprocess.check_call(f'tabix -f {output_name}.gz', shell=True,
                           executable='/bin/bash')
 
-
 def main():
-    parser = argparse.ArgumentParser(description='Generate normalized expression BED files for eQTL analyses')
-    parser.add_argument('norm_gct',
-                        help='GCT file with normalized expression')
+    parser = argparse.ArgumentParser(
+        description='Generate normalized methylation BED files for mQTL analyses'
+    )
+    parser.add_argument('vmr_matrix',
+                        help='Tab-delimited file with VMR methylation values (feature_id x samples)')
     parser.add_argument('sample_participant_lookup',
-                        help='Lookup table linking samples to participants')
-    parser.add_argument('vcf_chr_list', help='List of chromosomes in VCF')
-    parser.add_argument('prefix', help='Prefix for output file names')
-    parser.add_argument('-o', '--output_dir', default='.',
+                        help='Lookup table linking VMR sample IDs to participant IDs')
+    parser.add_argument('vcf_chr_list',
+                        help='File with list of chromosomes in VCF')
+    parser.add_argument('prefix',
+                        help='Prefix for output file names')
+    parser.add_argument('--output_dir', '-o', default='.',
                         help='Output directory')
     parser.add_argument('--sample_id_list', default=None,
-                        help='File listing sample IDs to include')
-    parser.add_argument('--feature', default='gene', help='gene, transcript or exon')
-    parser.add_argument('--bed_file', help='this is the bed file annotation')
-    parser.add_argument('--flip', action="store_false",
-                        help='Flip TSS using strand information')
+                        help='Optional file listing sample IDs to include')
+    parser.add_argument('--bed_file',
+                        help='BED file annotation with feature_id coordinates',
+                        required=True)
     args = parser.parse_args()
 
-    print('Loading expression data', flush=True)
+    print('Loading VMR methylation data', flush=True)
     sample_ids = None
-
     if args.sample_id_list is not None:
         with open(args.sample_id_list) as f:
             sample_ids = f.read().strip().split('\n')
-            print(f'  * Loading {len(sample_ids)} samples', flush=True)
+            print(f'  * Restricting to {len(sample_ids)} samples', flush=True)
 
-    norm_df = read_gct(args.norm_gct, sample_ids)
-    print(f'Map data', flush=True)
-    sample_participant_lookup_s = pd.read_csv(args.sample_participant_lookup,
-                                              sep='\t', index_col=0, dtype=str)\
-                                    .squeeze("columns")
-    norm_df = sort_samples(norm_df, sample_participant_lookup_s, sort=False)
-    print(f'  * {norm_df.shape[0]} genes.', flush=True)
+    vmr_df = read_vmr(args.vmr_matrix, sample_ids)
+    print(f'  * Loaded {vmr_df.shape[0]} features across {vmr_df.shape[1]} samples', flush=True)
 
-    # change sample IDs to participant IDs
-    norm_df.rename(columns=sample_participant_lookup_s.to_dict(), inplace=True)
-    bed_template_df = get_bed(args.bed_file, args.flip)
-    print('bed_template_df.shape', bed_template_df.shape, flush=True)
+    print('Mapping sample IDs to participants', flush=True)
+    sample_participant_lookup_s = pd.read_csv(
+        args.sample_participant_lookup,
+        sep='\t', index_col=0, dtype=str
+    ).squeeze("columns")
+
+    vmr_df = sort_samples(vmr_df, sample_participant_lookup_s, sort=False)
+    vmr_df.rename(columns=sample_participant_lookup_s.to_dict(), inplace=True)
+
+    bed_template_df = get_bed(args.bed_file)
+    print('  * Annotation loaded:', bed_template_df.shape, flush=True)
+
     with open(args.vcf_chr_list) as f:
         chr_list = f.read().strip().split('\n')
-    norm_bed_df = prepare_bed(norm_df, bed_template_df, chr_subset=chr_list)
-    print(f'  * {norm_bed_df.shape[0]} genes remain after removing contigs absent from VCF.', flush=True)
-    print('Writing BED file', flush=True)
-    write_bed(norm_bed_df, os.path.join(args.output_dir,
-                                        args.prefix+'.expression.bed'))
+
+    vmr_bed_df = prepare_bed(vmr_df, bed_template_df, chr_subset=chr_list)
+    print(f'  * {vmr_bed_df.shape[0]} features remain after restricting to VCF contigs', flush=True)
+
+    out_file = os.path.join(args.output_dir, args.prefix + '.methylation.bed')
+    print('Writing BED file:', out_file, flush=True)
+    write_bed(vmr_bed_df, out_file)
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
