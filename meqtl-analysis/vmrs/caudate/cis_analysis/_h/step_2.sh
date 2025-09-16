e91f6f4c31682f2d4971a9b62d611530e3b84acc
Alexis Bennett <alexis.bennett@northwestern.edu>
2025-09-16 14:00:14 -0500

    Update to use conda instead of mamba

--- meqtl-analysis/vmrs/caudate/cis_analysis/_h/step_2.sh
+++ meqtl-analysis/vmrs/caudate/cis_analysis/_h/step_2.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu:a100:1
 #SBATCH --job-name=permutation_eqtl
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=sierramannion2028@u.northwestern.edu ## Update!
+#SBATCH --mail-user=alexis.bennett@northwestern.edu
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=40gb
@@ -34,16 +34,15 @@ module load cuda/11.6.2-gcc-12.3.0
 module list
 
 # Set path variables
-log_message "**** Loading mamba environment ****" ## Update from mamba to conda!
-source /projects/p32505/opt/miniforge3/etc/profile.d/conda.sh
-eval "$(mamba shell hook --shell bash)"
+log_message "**** Loading conda environment ****"
+#source /projects/p32505/opt/miniforge3/etc/profile.d/conda.sh
 
 ENV_PATH="/projects/p32505/opt/env/eQTL_env"
 
-mamba run -p ${ENV_PATH} python ../_h/02.permutation_analysis.py
+conda run -p ${ENV_PATH} python ../_h/02.permutation_analysis.py
 
 if [ $? -ne 0 ]; then
-    log_message "Error: mamba or script execution failed"; exit 1
+    log_message "Error: conda or script execution failed"; exit 1
 fi
 
 log_message "**** Job ends ****"
