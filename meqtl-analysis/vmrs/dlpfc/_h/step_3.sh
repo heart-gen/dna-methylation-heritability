0fe24531de6a5e4fdbe48ad93e122a1a7361e3f3
Alexis Bennett <alexis.bennett@northwestern.edu>
2025-09-18 12:41:55 -0500

    Adapt eqtl prep scripts for the dlpfc and hippocampus; closes #9

---
+++ meqtl-analysis/vmrs/dlpfc/_h/step_3.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#SBATCH --account=p32505
+#SBATCH --partition=short
+#SBATCH --job-name=format_expression
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=alexis.bennett@northwestern.edu
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=10gb
+#SBATCH --output=logs/formatting.%j.log
+#SBATCH --time=00:30:00
+
+# Function to echo with timestamp
+log_message() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
+}
+
+log_message "**** Job starts ****"
+
+log_message "**** Quest info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOBID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURM_NODENAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID:-N/A}"
+
+## List current modules for reproducibility
+log_message "**** Loading modules ****"
+
+module purge
+module load htslib/1.16
+module list
+
+# Set path variables
+log_message "**** Loading conda environment ****"
+ENV_PATH="/projects/p32505/opt/env"
+
+conda run -p $ENV_PATH/AI_env \
+      python ../_h/03.prepare_expression.py \
+      ./normalized_methylation.tsv \
+      ./vcf_chr_list.txt vmrs \
+      -o ./ --bed_file ./feature.bed \
+      --skip_sample_lookup \
+      --sample_id_list ./sample_brnum.txt
+      
+if [ $? -ne 0 ]; then
+    log_message "Error: Python script execution failed"
+    exit 1
+fi
+
+log_message "**** Job ends ****"
---
+++ meqtl-analysis/vmrs/hippocampus/_h/04.generate_covs.R
@@ -0,0 +1,72 @@
+## Prepare covariates for FastQTL / tensorQTL analysis
+## Requires the 'sva' package
+suppressPackageStartupMessages({
+    library(here)
+    library(dplyr)
+    library(SummarizedExperiment)
+    library('data.table')
+})
+
+#### Functions
+format_pheno <- function(pheno_file_path) {
+    pheno_df <- fread(pheno_file_path, header = TRUE) |>
+        filter(race == "AA", agedeath >= 17, region == "hippocampus") |>
+        select(brnum, agedeath, sex, primarydx)
+    
+                                        # One hot encode categorical covs
+    one_hot_df <- model.matrix(~ primarydx + sex - 1, data = pheno_df) |>
+        as.data.frame() |>
+        select(-primarydxControl)
+    
+    pheno <- cbind(BrNum = pheno_df$brnum, 
+                   one_hot_df, 
+                   Age = pheno_df$agedeath)
+  return(pheno)
+}
+
+get_covs <- function(pheno, sample_id) {
+                                      # Subset samples
+    sample_df <- data.table::fread(sample_id)
+  
+                                      # Extract covariates
+    covs_df <- pheno |>
+      inner_join(sample_df, by = "BrNum") |>
+      tibble::column_to_rownames("BrNum")
+    
+                                      # Reformat covariates
+    covs <- t(covs_df) |>
+      as.data.frame() |>
+      tibble::rownames_to_column("ID") |>
+      select(c("ID", sample_df$BrNum))
+    
+    return(covs)
+}
+
+#### Main
+
+                                       # Create output dir
+output_path <- here("meqtl-analysis", "vmrs", "hippocampus", "_m")
+
+if (!dir.exists(output_path)) {
+    dir.create(output_path, recursive = TRUE)
+}
+
+                                        # Get pheno file for covariates
+pheno_file_path <- here("inputs", "phenotypes", "_m", "phenotypes-AA.tsv")
+pheno <- format_pheno(pheno_file_path)
+
+
+                                        # Extract covariates
+sample_id <- here("meqtl-analysis", "vmrs", "hippocampus", "_m", 
+                  "sample_brnum.txt")
+covs <- get_covs(pheno, sample_id)
+out_covs   <- file.path(output_path, "vmrs.combined_covariates.txt")
+
+                                        # Save file
+data.table::fwrite(covs, out_covs, sep='\t')
+
+## Reproducibility
+Sys.time()
+proc.time()
+options(width = 120)
+sessioninfo::session_info()
