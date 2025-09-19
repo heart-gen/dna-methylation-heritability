2101884b39c08fd1265b81c41e576059600fc135
Elisa Johnson <elisajohnson2027@u.northwestern.edu>
2025-09-19 13:29:38 -0500

    Adds scripts to calculate and summarize statistics for the hippocampus dataset

---
+++ testing/hippocampus/_h/stats/03.calculate_age.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -A p32505
+#SBATCH -p short
+#SBATCH -t 01:00:00
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --mem=4G
+#SBATCH --job-name=calculate_age
+#SBATCH --output=logs/calculate_age.out
+#SBATCH --error=logs/calculate_age.err
+
+python /projects/p32505/users/elisa/testing/hippocampus/_h/03.calculate_age.py
