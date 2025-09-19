b9c6147ccbb1517aa2310fba8b7711ce4922351a
Elisa Johnson <elisajohnson2027@u.northwestern.edu>
2025-09-19 13:26:16 -0500

    Adds scripts to calculate and summarize statistics for the caudate dataset.

---
+++ testing/caudate/_h/stats/02.count_sexes.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -A p32505
+#SBATCH -p short
+#SBATCH -t 01:00:00
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --mem=4G
+#SBATCH --job-name=count_sexes
+#SBATCH --output=logs/count_sexes.out
+#SBATCH --error=logs/count_sexes.err
+
+python /projects/p32505/users/elisa/testing/caudate/_h/count_sexes.py
