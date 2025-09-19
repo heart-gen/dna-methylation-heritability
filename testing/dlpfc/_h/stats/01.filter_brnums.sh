01ab3fe9afc6bf4d5207973682895147962155b4
Elisa Johnson <elisajohnson2027@u.northwestern.edu>
2025-09-19 13:29:04 -0500

    Adds scripts to calculate and summarize statistics for the dlpfc dataset.

---
+++ testing/dlpfc/_h/stats/01.filter_brnums.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -A p32505
+#SBATCH -p short
+#SBATCH -t 01:00:00
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --mem=4G
+#SBATCH --job-name=filter_brnums
+#SBATCH --output=logs/filter_brnums.out
+#SBATCH --error=logs/filter_brnums.err
+
+python /projects/p32505/users/elisa/testing/dlpfc/_h/filter_brnums.py
