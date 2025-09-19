00b3f941c0c267d48841f70d987d7a9c43ff32b5
Elisa Johnson <elisajohnson2027@u.northwestern.edu>
2025-09-19 13:29:04 -0500

    Adds scripts to calculate and summarize statistics for the dlpfc dataset.

---
+++ testing/dlpfc/_h/stats/04.phenotype_percentages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -A p32505
+#SBATCH -p short
+#SBATCH -t 01:00:00
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --mem=4G
+#SBATCH --job-name=phenotype_percentages
+#SBATCH --output=logs/phenotype_percentages.out
+#SBATCH --error=logs/phenotype_percentages.err
+
+python /projects/p32505/users/elisa/testing/dlpfc/_h/phenotype_percentages.py
