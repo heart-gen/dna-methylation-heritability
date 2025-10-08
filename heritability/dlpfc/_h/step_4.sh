24440b91658765c84a49833d4018227b7db9d14e
Alexis Bennett <alexis.bennett@northwestern.edu>
2025-10-08 16:46:07 -0500

    Update array job for new vmr list

--- heritability/dlpfc/_h/step_4.sh
+++ heritability/dlpfc/_h/step_4.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks-per-node=1     # Number of cores (CPU)
 #SBATCH --mem=10G               # Memory limit
 #SBATCH --mail-type=FAIL
-#SBATCH --array=1-9513%250
+#SBATCH --array=1-9977%250
 #SBATCH --mail-user=alexis.bennett@northwestern.edu
 #SBATCH --job-name=cal_vmr  # Job name
 #SBATCH --output=/dev/null      # Standard output log
