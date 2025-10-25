e950f9d28dc05fd22a9496b189074fb6e54a944c
Elisa Johnson <elisajohnson2027@u.northwestern.edu>
2025-10-24 23:32:22 -0500

    Adds logging information

---
+++ heritability/elastic_net_model/tissue_comparison/functional_enrichment/s-ldsc/_h/00.format_plink.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --account=p32505        # Replace with your allocation
+#SBATCH --partition=short       # Partition (queue) name
+#SBATCH --time=01:00:00         # Time limit hrs:min:sec
+#SBATCH --nodes=1               # Number of nodes
+#SBATCH --ntasks-per-node=1     # Number of cores (CPU)
+#SBATCH --mem=1G                # Memory limit
+#SBATCH --job-name=02.multi_core  # Job name
+#SBATCH --output=output_%j.log  # Standard output log
+#SBATCH --error=error_%j.log    # Standard error log
+
+# Log function
+log_message() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
+}
+
+log_message "**** Job starts ****"
+
+echo "**** QUEST info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOBID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURM_NODENAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID:-N/A}"
+
+log_message "**** Job ends ****"
