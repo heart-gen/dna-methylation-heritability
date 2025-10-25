90d79bb97560ab75800160972c66eec9bd9c9438
Elisa Johnson <elisajohnson2027@u.northwestern.edu>
2025-10-25 12:30:30 -0500

    Adds ldsc.py block

--- heritability/elastic_net_model/tissue_comparison/functional_enrichment/s-ldsc/_h/02.compute_ldscores.sh
+++ heritability/elastic_net_model/tissue_comparison/functional_enrichment/s-ldsc/_h/02.compute_ldscores.sh
@@ -5,6 +5,7 @@
 #SBATCH --nodes=1               # Number of nodes
 #SBATCH --ntasks-per-node=1     # Number of cores (CPU)
 #SBATCH --mem=25G                # Memory limit
+#SBATCH --array=1-22            # Array job for chromosomes 1-22
 #SBATCH --job-name=compute_ldscores  # Job name
 #SBATCH --output=logs/compute_ldscores/output_%j.log  # Standard output log
 #SBATCH --error=logs/compute_ldscores/error_%j.log    # Standard error log
@@ -22,6 +23,18 @@ echo "Job id: ${SLURM_JOBID}"
 echo "Job name: ${SLURM_JOB_NAME}"
 echo "Node name: ${SLURM_NODENAME}"
 echo "Hostname: ${HOSTNAME}"
-echo "Task id: ${SLURM_ARRAY_TASK_ID:-N/A}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+output_dir="custom_ldscores"
+mkdir -p ${output_dir}
+
+python ldsc.py \
+	--l2 \
+	--bfile /projects/p32505/users/elisa/dna-methylation-heritability/heritability/elastic_net_model/tissue_comparison/functional_enrichment/s-ldsc/_m/plink_files/chr_${SLURM_ARRAY_TASK_ID} \
+	--ld-wind-cm 1 \
+	--annot /projects/p32505/users/elisa/dna-methylation-heritability/heritability/elastic_net_model/tissue_comparison/functional_enrichment/s-ldsc/_m/annotations/chr_${SLURM_ARRAY_TASK_ID}.annot.gz \
+	--thin-annot \
+	--out $output_dir/caudate \
+	--print-snps /projects/p32505/users/elisa/dna-methylation-heritability/heritability/elastic_net_model/tissue_comparison/functional_enrichment/s-ldsc/w_hm3.snplist.gz
 
 log_message "**** Job ends ****"
