a9662185bbaf4777557c494b0be4209b2ec57b1c
Elisa Johnson <elisajohnson2027@u.northwestern.edu>
2025-10-25 18:05:34 -0500

    Updates output file path

--- heritability/elastic_net_model/tissue_comparison/functional_enrichment/s-ldsc/_h/01.make_annot.sh
+++ heritability/elastic_net_model/tissue_comparison/functional_enrichment/s-ldsc/_h/01.make_annot.sh
@@ -32,7 +32,7 @@ for chr in {1..22}; do
     python /projects/p32505/users/elisa/dna-methylation-heritability/heritability/elastic_net_model/tissue_comparison/functional_enrichment/s-ldsc/ldsc/make_annot.py \
         --bed-file /projects/p32505/users/elisa/dna-methylation-heritability/heritability/elastic_net_model/tissue_comparison/functional_enrichment/s-ldsc/custom_annotations/caudate/caudate_vmr_gene_annotation.${chr}.bed \
         --bimfile /projects/p32505/users/elisa/dna-methylation-heritability/heritability/elastic_net_model/tissue_comparison/functional_enrichment/s-ldsc/1000G_EUR_Phase3_plink/1000G.EUR.QC.${chr}.bim \
-        --annot-file /projects/p32505/users/elisa/dna-methylation-heritability/heritability/elastic_net_model/tissue_comparison/functional_enrichment/s-ldsc/custom_annotations/caudate/my_annotation.${chr}.annot.gz
+        --annot-file /projects/p32505/users/elisa/dna-methylation-heritability/heritability/elastic_net_model/tissue_comparison/functional_enrichment/s-ldsc/_m/caudate/annotations/caudate_vmr_gene_annotation.${chr}.annot.gz
 done
 
 log_message "**** Job ends ****"
