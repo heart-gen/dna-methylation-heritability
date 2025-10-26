e5a8c63ca6d91a3f3879dbc7b907a11938921e5d
Elisa Johnson <elisajohnson2027@u.northwestern.edu>
2025-10-25 20:23:06 -0500

    Adds new script for making annotations in s-ldsc

---
+++ heritability/elastic_net_model/tissue_comparison/functional_enrichment/s-ldsc/_h/01.make_annot.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#SBATCH --account=p32505        # Replace with your allocation
+#SBATCH --partition=short       # Partition (queue) name
+#SBATCH --time=01:00:00         # Time limit hrs:min:sec
+#SBATCH --nodes=1               # Number of nodes
+#SBATCH --ntasks-per-node=1     # Number of cores (CPU)
+#SBATCH --mem=1G                # Memory limit
+#SBATCH --job-name=make_annot  # Job name
+#SBATCH --output=output_%j.log  # Standard output log
+#SBATCH --error=error_%j.log    # Standard error log
+
+# Log function
+log_message() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
+}
+
+log_message "**** Job starts ****"
+
+echo "**** QUEST info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOBID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURM_NODENAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID:-N/A}"
+
+module load bedtools/2.30.0
+
+mkdir -p annotation_files
+
+python make_annot.py \
+	--bed-file /projects/p32505/users/elisa/dna-methylation-heritability/heritability/elastic_net_model/tissue_comparison/functional_enrichment/s-ldsc/bedfiles/${chr}_caudate_vmr_gene_annotation.bed \
+	--bimfile /projects/p32505/users/elisa/dna-methylation-heritability/heritability/elastic_net_model/tissue_comparison/functional_enrichment/s-ldsc/GRCh38/plink_files//projects/p32505/users/elisa/dna-methylation-heritability/heritability/elastic_net_model/tissue_comparison/functional_enrichment/s-ldsc/GRCh38/plink_files/1000G.EUR.hg38.${chr}.bim \
+	--annot-file /projects/p32505/users/elisa/dna-methylation-heritability/heritability/elastic_net_model/tissue_comparison/functional_enrichment/s-ldsc/_m/annotation_files/${chr}_caudate_vmr_gene_annotation.annot.gz \
+    --windowsize 500000
+
+log_message "**** Job ends ****"
