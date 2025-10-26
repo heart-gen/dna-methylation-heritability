fcdd17787eba157d9684211ef37cf24df09faae0
Elisa Johnson <elisajohnson2027@u.northwestern.edu>
2025-10-25 20:49:25 -0500

    Update input file paths

--- heritability/elastic_net_model/tissue_comparison/functional_enrichment/s-ldsc/_h/01.make_annot.sh
+++ heritability/elastic_net_model/tissue_comparison/functional_enrichment/s-ldsc/_h/01.make_annot.sh
@@ -32,9 +32,9 @@ for chr in {1..22}; do
 	echo "Processing chromosome ${chr}"
 
 	python /projects/p32505/users/elisa/dna-methylation-heritability/heritability/elastic_net_model/tissue_comparison/functional_enrichment/s-ldsc/ldsc/make_annot.py \
-		--bed-file /projects/p32505/users/elisa/dna-methylation-heritability/heritability/elastic_net_model/tissue_comparison/functional_enrichment/s-ldsc/bed_files/${chr}_caudate_vmr_gene_annotation.bed \
+		--bed-file /projects/p32505/users/elisa/dna-methylation-heritability/heritability/elastic_net_model/tissue_comparison/functional_enrichment/s-ldsc/bed_files/${chr}_hippocampus_vmr_gene_annotation.bed \
 		--bimfile /projects/p32505/users/elisa/dna-methylation-heritability/heritability/elastic_net_model/tissue_comparison/functional_enrichment/s-ldsc/GRCh38/plink_files/1000G.EUR.hg38.${chr}.bim \
-		--annot-file /projects/p32505/users/elisa/dna-methylation-heritability/heritability/elastic_net_model/tissue_comparison/functional_enrichment/s-ldsc/_m/annotation_files/${chr}_caudate_vmr_gene_annotation.annot.gz \
+		--annot-file /projects/p32505/users/elisa/dna-methylation-heritability/heritability/elastic_net_model/tissue_comparison/functional_enrichment/s-ldsc/_m/annotation_files/${chr}_hippocampus_vmr_gene_annotation.annot.gz \
     	--windowsize 500000
 
 done
