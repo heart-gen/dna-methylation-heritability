2ba1b4108701a8f5b85f0a175b29a88b1a49945d
Alexis Bennett <alexis.bennett@northwestern.edu>
2025-10-30 12:57:22 -0500

    Adding fixed parameters for dask orchestration and testing all phenotypes

--- simulation-analysis/GENboostGPU/sim_100_indiv/_h/step_1.sh
+++ simulation-analysis/GENboostGPU/sim_100_indiv/_h/step_1.sh
@@ -10,7 +10,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=40G
-#SBATCH --time=02:00:00
+#SBATCH --time=10:00:00
 
 log_message() {
     echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
