5945f25c960bb103b6270eca932f0ae434846b7a
Alexis Bennett <alexis.bennett@northwestern.edu>
2025-11-06 12:21:05 -0600

    Copy meqtl analysis scripts

---
+++ covar-analysis/meqtl/global_ances/cis_analysis/_h/01.eqtl_tensorqtl.py
@@ -0,0 +1,44 @@
+"""This script runs tensorQTL in Python."""
+import pandas as pd
+import session_info
+from functools import lru_cache
+from tensorqtl import cis, pgen, read_phenotype_bed
+
+@lru_cache()
+def get_genotypes():
+    plink_prefix_path = "../../_m/protected_data/TOPMed_LIBD"
+    pgr = pgen.PgenReader(plink_prefix_path)
+    variant_df = pgr.variant_df
+    variant_df.loc[:, "chrom"] = "chr" + variant_df.chrom
+    return pgr.load_genotypes(), variant_df
+
+
+@lru_cache()
+def get_covars(feature = "vmrs"):
+    covar_file = f"../../_m/{feature}.combined_covariates.txt"
+    return pd.read_csv(covar_file, sep='\t', index_col=0).T
+
+
+@lru_cache()
+def get_phenotype(feature = "vmrs"):
+    meth_bed = f"../../_m/{feature}.methylation.bed.gz"
+    return read_phenotype_bed(meth_bed)
+
+
+def main():
+    # Load data
+    feature = "vmrs"; prefix = "TOPMed_LIBD"
+    phenotype_df, phenotype_pos_df = get_phenotype(feature)
+    genotype_df, variant_df = get_genotypes()
+
+    # Nominal
+    cis.map_nominal(genotype_df, variant_df, phenotype_df,
+                    phenotype_pos_df, prefix, covariates_df=get_covars(feature),
+                    maf_threshold=0.01, window=500000, output_dir=".")
+
+    # Session information
+    session_info.show()
+
+
+if __name__ == "__main__":
+    main()
