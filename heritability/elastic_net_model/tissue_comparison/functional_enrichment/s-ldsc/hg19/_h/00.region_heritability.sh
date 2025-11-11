c308b0b650ccaf1a452dd2219a72f08d4e3aaadd
Alexis Bennett <alexis.bennett@northwestern.edu>
2025-11-11 09:47:38 -0600

    Add scripts using hg19 build; Update pipeline to use relative paths

---
+++ heritability/elastic_net_model/tissue_comparison/functional_enrichment/s-ldsc/hg19/_h/00.region_heritability.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#SBATCH --account=p32505
+#SBATCH --partition=short
+#SBATCH --time=01:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=10G
+#SBATCH --job-name=region_heritability
+#SBATCH --output=logs/00.region_heritability_output.log
+#SBATCH --error=logs/00.region_heritability_error.log
+
+# Log function
+log_message() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
+}
+
+log_message "**** Job starts ****"
+
+echo "**** QUEST info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOBID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURM_NODENAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID:-N/A}"
+
+conda activate /projects/p32505/opt/envs/ml
+
+BRAIN_REGIONS=("caudate" "dlpfc" "hippocampus")
+
+for REGION in "${BRAIN_REGIONS[@]}"; do
+
+    PYTHON_SCRIPT="../_h/00.region_heritability.py"
+    INPUT_FILE="/projects/b1213/users/alexis/projects/dna-methylation-heritability/heritability/elastic_net_model/${REGION}/_m/${REGION}_summary_elastic-net.tsv"
+    OUTPUT_DIR="./vmr/${REGION}"
+    mkdir -p "$OUTPUT_DIR"
+
+    echo "Running separation script for region: $REGION"
+    python "$PYTHON_SCRIPT" \
+        --input_file "$INPUT_FILE" \
+        --output_dir "$OUTPUT_DIR"
+
+    echo "Separation completed. Check files in $OUTPUT_DIR"
+
+done
+
+conda deactivate
+log_message "**** Job ends ****"
