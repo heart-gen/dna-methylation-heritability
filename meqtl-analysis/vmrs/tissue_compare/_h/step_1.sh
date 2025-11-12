45ca9fd69b7060a4b1812418f8b235d36b3076a6
Alexis Bennett <alexis.bennett@northwestern.edu>
2025-11-12 10:59:42 -0600

    Add submission script for prepping nominal localqtl data

---
+++ meqtl-analysis/vmrs/tissue_compare/_h/step_1.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#SBATCH --account=b1042
+#SBATCH --partition=genomics
+#SBATCH --job-name=prep_nominal
+#SBATCH --mail-type=FAIL ## If you want to have it email you for any reason
+#SBATCH --mail-user=alexis.bennett@northwestern.edu ## replace with your email
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=50gb
+#SBATCH --output=logs/prepaprep_nominalre.%j.log
+#SBATCH --time=10:00:00
+
+# Function to echo with timestamp
+log_message() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
+}
+
+log_message "**** Job starts ****"
+
+log_message "**** Quest info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOBID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURM_NODENAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID:-N/A}"
+
+## List current modules for reproducibility
+log_message "**** Loading modules ****"
+
+module purge
+module list
+
+# Set path variables
+log_message "**** Loading conda environment ****"
+
+source /projects/p32505/opt/miniforge3/etc/profile.d/conda.sh
+conda activate /projects/p32505/opt/envs/genomics
+python ../_h/01.prepare_nominal.py --localqtl
+
+if [ $? -ne 0 ]; then
+    log_message "Error: Conda or script execution failed"
+    exit 1
+fi
+
+conda deactivate
+
+log_message "**** Job ends ****"
