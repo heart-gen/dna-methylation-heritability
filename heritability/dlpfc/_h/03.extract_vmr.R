8aca8dc7d1a0bd0c1c0b51afe6905f77a9ab4d6b
Alexis Bennett <alexis.bennett@northwestern.edu>
2025-11-20 12:24:17 -0600

    Adapt finalized pipeline for dlpfc

--- heritability/dlpfc/_h/03.extract_vmr.R
+++ heritability/dlpfc/_h/03.extract_vmr.R
@@ -8,23 +8,23 @@ suppressPackageStartupMessages({
 args <- commandArgs(trailingOnly = TRUE)
 chr  <- args[1]
 
-# Function
+## Function
 get_vmr <- function(v, out_vmr) {
-  sdCut  <- quantile(v[, 3], prob = 0.99, na.rm = TRUE)
-  vmrs   <- c()
-  v2     <- v[v$chr==chr, ]
-  v2     <- v2[order(v2$start), ]
-  isHigh <- rep(0, nrow(v2))
-  isHigh[v2$sd > sdCut] <- 1
-  vmrs0  <- bsseq:::regionFinder3(isHigh, as.character(v2$chr), 
-                                  v2$start, maxGap = 1000)$up
-  vmr    <- vmrs0[vmrs0$n > 5,1:3]
-  write.table(vmr, file=out_vmr, col.names=F,
-              row.names=F, sep="\t", quote=F)
-  return(vmr)
+    sdCut  <- quantile(v[, 3], prob = 0.99, na.rm = TRUE)
+    vmrs   <- c()
+    v2     <- v[v$chr==chr, ]
+    v2     <- v2[order(v2$start), ]
+    isHigh <- rep(0, nrow(v2))
+    isHigh[v2$sd > sdCut] <- 1
+    vmrs0  <- bsseq:::regionFinder3(isHigh, as.character(v2$chr), 
+                                    v2$start, maxGap = 1000)$up
+    vmr    <- vmrs0[vmrs0$n > 5,1:3]
+    write.table(vmr, file=out_vmr, col.names=F,
+                row.names=F, sep="\t", quote=F)
+    return(vmr)
 }
 
-# Main
+## Main
 
                                         # create output dir if doesn't exist
 output_path <- here("heritability/dlpfc/_m/vmr", paste0("chr_", chr))
