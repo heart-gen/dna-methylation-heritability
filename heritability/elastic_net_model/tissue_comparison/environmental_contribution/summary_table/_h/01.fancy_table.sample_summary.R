03eeebfa7455acdd1d39b32f132f3391671036aa
Alexis Bennett <alexis.bennett@northwestern.edu>
2025-12-01 16:00:17 -0600

    Modifying script to generate table for environmental factors

--- heritability/elastic_net_model/tissue_comparison/environmental_contribution/summary_table/_h/01.fancy_table.sample_summary.R
+++ heritability/elastic_net_model/tissue_comparison/environmental_contribution/summary_table/_h/01.fancy_table.sample_summary.R
@@ -1,48 +1,66 @@
-## Summarize results and plot correlation with q-value
+## Summarize environmental factors by brain region
 
 suppressPackageStartupMessages({
     library(here)
     library(dplyr)
     library(gtsummary)
+    library(data.table)
 })
 
 options(gt.html_tag_check = FALSE)
 
+## Function
+
 save_table <- function(pp, fn){
-    for(ext in c(".pdf", ".tex", ".rtf")){
+    for(ext in c(".tex", ".rtf")){
         gt::gtsave(as_gt(pp), filename=paste0(fn,ext))
     }
 }
 
-load_data <- function(){
-    fn <- here("inputs/counts/_m",
-               "RNAseq_Collection_postQC_n2844_6publicDataset_2021-01_geneRSE.Rdata")
-    load(fn)
-    sample_data <- SummarizedExperiment::colData(rse_gene) |> as.data.frame() |>
-        filter(Dx %in% c("MDD", "Control"), Region == "DLPFC", Age > 17,
-               Race %in% c("AA", "CAUC"), Dataset == "BrainSeq_Phase1") |>
-        select(!c("bamFile", "rna_preSwap_BrNum")) |>
-        mutate(Race=recode(Race, "AA"="BA", "CAUC"="WA")) |>
-        mutate_if(is.character, as.factor)
-    return(sample_data)
+clean_table <- function(pheno_file_path, vars){
+    pheno_df <- fread(pheno_file_path, header = TRUE) |>
+    select(all_of(vars_to_include)) |>
+    filter(agedeath > 17) |>
+    mutate(region = recode(region,
+                           "caudate" = "Caudate",
+                           "dlpfc" = "DLPFC",
+                           "hippocampus" = "Hippocampus")) |>
+    mutate_if(is.character, as.factor)
+
+    return(pheno_df)
 }
 
-#### MAIN
+## Main
+                                        # Get variables of interest
+vars_to_include <- c(
+  "agedeath", "sex","primarydx","region","smoking","codeine","morphine",
+  "cocaine","ethanol","antipsychotics","nicotine","amphetamines",
+  "education","marital_status","hx_sexual_abuse","hx_physical_abuse",
+  "hx_other_trauma","hx_military_service","fsiq"
+)
+
                                         # Generate phenotype data
-pheno_df <- load_data()
-data.table::fwrite(pheno_df, file="phenotype_data.tsv", sep="\t",
-                   row.names=FALSE, col.names=TRUE)
+pheno_file_path <- here("inputs/phenotypes/_m/phenotypes-AA.tsv")
+pheno_df <- clean_table(pheno_file_path, vars_to_include)
 
                                          # Generate pretty tables
-fn <- "sample_breakdown.table"
-pp <- pheno_df |> select(Dx, Race, Sex, Age, RIN) |>
-    mutate(Sex=factor(Sex, labels=c("Female", "Male")),
-           Race=factor(forcats::fct_drop(Race), labels=c("BA", "WA")),
-           Dx=factor(forcats::fct_drop(Dx), labels=c("CTL", "MDD"))) |>
-    tbl_summary(by="Dx", missing="no",
+fn <- "environmental_factors.table"
+pp <- pheno_df |> select(!c("agedeath")) |>
+    mutate(sex=factor(sex, labels=c("Female", "Male")),
+           primarydx=factor(forcats::fct_drop(primarydx), labels=c("CTL", "SCZ"))) |>
+    tbl_summary(by="region", missing="no",
+                label = list(
+                  sex ~ "Sex", primarydx ~ "Diagnosis", smoking ~ "Smoking",
+                  codeine ~ "Codeine", morphine ~ "Morphine", cocaine ~ "Cocaine",
+                  ethanol ~ "Ethanol", amphetamines ~ "Amphetamines",
+                  antipsychotics ~ "Antipsychotics", nicotine ~ "Nicotine",
+                  hx_military_service ~ "Military Service", hx_other_trauma ~ "Trauma",
+                  hx_physical_abuse ~ "Physical Abuse", hx_sexual_abuse ~ "Sexual Abuse",
+                  education ~ "Education", marital_status ~ "Marital Status",
+                  fsiq ~ "FSIQ"),
                 statistic=all_continuous() ~ c("{mean} ({sd})")) |>
     modify_header(all_stat_cols()~"**{level}**<br>N = {n}") |>
-    modify_spanning_header(all_stat_cols()~"**Diagnosis**") |>
+    modify_spanning_header(all_stat_cols()~"**Brain Region**") |>
     bold_labels() |> italicize_levels()
 save_table(pp, fn)
 
