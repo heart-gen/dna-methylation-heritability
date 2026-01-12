ac0676ca388352bb03109cc815161bb2c022a19c
Alexis Bennett <alexis.bennett@northwestern.edu>
2026-01-12 15:12:53 -0600

    Convert pca plots to use ggplot

--- heritability/elastic_net_model/tissue_comparison/annotation/enrichment/_h/03.clustering.R
+++ heritability/elastic_net_model/tissue_comparison/annotation/enrichment/_h/03.clustering.R
@@ -6,6 +6,8 @@ suppressPackageStartupMessages({
   library(data.table)
   library(tidyverse)
   library(dendextend)
+  library(M3C)
+  library(ggplot2)
 })
 
 # Function 
@@ -25,6 +27,12 @@ gen_data <- function(){
   return(dt)
 }
 
+save_plot <- function(p, fn, w, h, dpi){
+  for(ext in c(".pdf", ".png")){
+    ggsave(filename=paste0(fn,ext), plot=p, width=w, height=h, dpi=dpi)
+  }
+}
+
 plot_dendrogram <- function(df, tissue, h2_cols, output_path) {
   tissue_df <- df %>%
     filter(Tissue == tissue)
@@ -38,7 +46,7 @@ plot_dendrogram <- function(df, tissue, h2_cols, output_path) {
   labels(hc) <- tissue_df$Annotation
   labels_colors(hc) <- h2_cols[tissue_df$h2_Category]
   
-  pdf(file = file.path(output_path, paste0(tissue, "_hc.pdf")))
+  pdf(file = file.path(output_path, paste0(tolower(tissue), "_hc.pdf")))
   plot(hc, cex = 0.7)
   dev.off()
   
@@ -48,15 +56,37 @@ plot_dendrogram <- function(df, tissue, h2_cols, output_path) {
 pca <- function(df, tissue, h2_cols, output_path) {
   filtered <- df %>%
     filter(Tissue == tissue) %>%
-    select("log2(OR)", "-log10(FDR)")
-  pc <- prcomp(filtered, scale = TRUE)
+    select("log2(OR)", "-log10(FDR)", h2_Category)
+  pc <- prcomp(filtered[, c("log2(OR)", "-log10(FDR)")], scale = TRUE)
   print(summary(pc))
+
+  # format df for plotting 
+  pc_df <- as.data.frame(pc$x[ , 1:2])
+  pc_df <- cbind(pc_df, filtered$h2_Category)
+  colnames(pc_df) <- c("PC1", "PC2", "Heritability Category")
   
-  pdf(file = file.path(output_path, paste0(tissue, "_pca.pdf")))
-  plot(pc$x[, 1], pc$x[, 2], col = h2_cols[df$h2_Category])
-  dev.off()
+  # plot pca
+  p <- pc_df %>%
+    ggplot(aes(x = PC1, y = PC2, 
+               color = `Heritability Category`, 
+               fill = `Heritability Category`, 
+               shape = `Heritability Category`)) +
+    geom_point(size = 5) +
+    labs(x = "PC1",
+         y = "PC2",
+         color = "Heritability Category",
+         shape = "Heritability Category") +
+    scale_fill_manual(values = h2_cols) +
+    scale_color_manual(values = h2_cols) +
+    theme_minimal(base_size = 20) +
+    theme(
+      legend.position = "right",
+      panel.grid.major = element_blank(), 
+      panel.grid.minor = element_blank(),
+      axis.line = element_line(colour = "black", linewidth = 1, linetype = "solid")
+    )
   
-  return(pc)
+  return(p)
 }
 
 # Main
@@ -96,13 +126,13 @@ h2_cols <- c(
 
 for (tissue in tissues) {
   hc <- plot_dendrogram(df, tissue, h2_cols, output_path)
-  pc <- pca(df, tissue, h2_cols, output_path)
+  p <- pca(df, tissue, h2_cols, output_path)
+  pca_fn <- file.path(output_path, paste0(tolower(tissue), "_pca"))
+  save_plot(p, pca_fn, w = 10, h = 6, dpi = 300)
 }
 
-dend_diff(hc_caudate, hc_dlpfc)
-tanglegram(hc_dlpfc, hc_hippo)
-
-# UMAP testing
+#dend_diff(hc_caudate, hc_dlpfc)
+#tanglegram(hc_dlpfc, hc_hippo)
 
 ## Reproducibility information
 Sys.time()
