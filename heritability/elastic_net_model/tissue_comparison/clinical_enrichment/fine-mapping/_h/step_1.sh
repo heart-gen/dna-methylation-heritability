1d949d0eee12b134abb38a5ecfd58294f7aa854a
Elisa Johnson <elisajohnson2027@u.northwestern.edu>
2026-01-20 11:42:31 -0600

    Adds new scripts for heritability fine-mapping analysis

---
+++ heritability/elastic_net_model/tissue_comparison/clinical_enrichment/fine-mapping/_h/step_1.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#SBATCH --account=p32505        # Replace with your allocation
+#SBATCH --partition=short       # Partition (queue) name
+#SBATCH --time=02:30:00         # Time limit hrs:min:sec
+#SBATCH --nodes=1               # Number of nodes
+#SBATCH --ntasks-per-node=1     # Number of cores (CPU)
+#SBATCH --mem=25G                # Memory limit
+#SBATCH --job-name=cal_ld  # Job name
+#SBATCH --output=logs/output_%j.log  # Standard output log
+#SBATCH --error=logs/error_%j.log    # Standard error log
+#SBATCH --array=1-22         # Array job range (if needed)
+
+# Log function
+log_message() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
+}
+
+log_message "**** Job starts ****"
+
+echo "**** QUEST info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOBID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURM_NODENAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID:-N/A}"
+
+mkdir -p cal_ld
+cd cal_ld
+mkdir -p chr_${SLURM_ARRAY_TASK_ID}
+cd ..
+
+REF_DIR=/projects/b1213/users/alexis/projects/dna-methylation-heritability/heritability/caudate/_m/plink_format/chr_${SLURM_ARRAY_TASK_ID}
+
+for file in "$REF_DIR"/TOPMed_LIBD.AA.*.bed; do
+	filename=$(basename "$file")
+
+	if [[ $filename =~ ([0-9]+)_([0-9]+)\. ]]; then
+		START=${BASH_REMATCH[1]}
+		END=${BASH_REMATCH[2]}
+		echo "File: $filename → START=$START, END=$END"
+	else
+		echo "File $filename does not match the expected pattern."
+	fi
+
+	START_POSITION=$((START - 500000))
+	END_POSITION=$((END + 500000))
+
+	python ../SuSiEx/utilities/SuSiEx_LD.py \
+		--ref_file=$REF_DIR/TOPMed_LIBD.AA.${START}_${END} \
+		--ld_file=cal_ld/chr_${SLURM_ARRAY_TASK_ID}/TOPMed_LIBD.AA.${START}_${END} \
+		--chr=${SLURM_ARRAY_TASK_ID} \
+		--bp=$START_POSITION,$END_POSITION \
+		--plink=../SuSiEx/utilities/plink \
+		--maf=0.005
+
+done
+
+log_message "**** Job ends ****"
