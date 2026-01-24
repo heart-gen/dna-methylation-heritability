365c145af2d7437469073b875332b32e6193a4f3
Elisa Johnson <elisajohnson2027@u.northwestern.edu>
2026-01-23 20:48:33 -0600

    Updates files paths in meqtl analysis scripts

--- meqtl-analysis/vmrs/caudate/local_analysis/_h/01.meqtl_localqtl.py
+++ meqtl-analysis/vmrs/caudate/local_analysis/_h/01.meqtl_localqtl.py
@@ -5,7 +5,7 @@ from localqtl.haplotypeio import RFMixReader
 from localqtl import PgenReader, read_phenotype_bed
 
 def get_genotypes():
-    plink_prefix_path = "/projects/b1213/users/alexis/projects/dna-methylation-heritability/meqtl-analysis/vmrs/hippocampus/_m/protected_data/TOPMed_LIBD"
+    plink_prefix_path = "/projects/b1213/users/alexis/projects/dna-methylation-heritability/meqtl-analysis/vmrs/caudate/_m/protected_data/TOPMed_LIBD"
     pgr = PgenReader(plink_prefix_path)
     variant_df = pgr.variant_df
     variant_df.loc[:, "chrom"] = "chr" + variant_df.chrom
@@ -14,12 +14,12 @@ def get_genotypes():
 
 
 def get_covars(feature = "vmrs"):
-    covar_file = f"/projects/b1213/users/alexis/projects/dna-methylation-heritability/meqtl-analysis/vmrs/hippocampus/_m/{feature}.combined_covariates.txt"
+    covar_file = f"/projects/b1213/users/alexis/projects/dna-methylation-heritability/meqtl-analysis/vmrs/caudate/_m/{feature}.combined_covariates.txt"
     return pd.read_csv(covar_file, sep='\t', index_col=0).T
 
 
 def get_phenotype(feature = "vmrs"):
-    meth_bed = f"/projects/b1213/users/alexis/projects/dna-methylation-heritability/meqtl-analysis/vmrs/hippocampus/_m/{feature}.methylation.bed.gz"
+    meth_bed = f"/projects/b1213/users/alexis/projects/dna-methylation-heritability/meqtl-analysis/vmrs/caudate/_m/{feature}.methylation.bed.gz"
     return read_phenotype_bed(meth_bed)
 
 
