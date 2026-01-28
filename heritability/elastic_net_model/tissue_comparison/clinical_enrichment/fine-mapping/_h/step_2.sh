8546d3a0c0d47dad54e4cd8a112ddb3f2e4037c7
Elisa Johnson <elisajohnson2027@u.northwestern.edu>
2026-01-28 12:41:55 -0600

    Updates step_2.sh to handle all chromosomes and different brain tissues

--- heritability/elastic_net_model/tissue_comparison/clinical_enrichment/fine-mapping/_h/step_2.sh
+++ heritability/elastic_net_model/tissue_comparison/clinical_enrichment/fine-mapping/_h/step_2.sh
@@ -5,9 +5,10 @@
 #SBATCH --nodes=1               # Number of nodes
 #SBATCH --ntasks-per-node=1     # Number of cores (CPU)
 #SBATCH --mem=25G                # Memory limit
-#SBATCH --job-name=run  # Job name
+#SBATCH --job-name=run_SuSiEx  # Job name
 #SBATCH --output=logs/output_%j.log  # Standard output log
 #SBATCH --error=logs/error_%j.log    # Standard error log
+#SBATCH --array=1-22          # Array job range
 
 # Log function
 log_message() {
@@ -24,32 +25,53 @@ echo "Node name: ${SLURM_NODENAME}"
 echo "Hostname: ${HOSTNAME}"
 echo "Task id: ${SLURM_ARRAY_TASK_ID:-N/A}"
 
-mkdir -p results/chr_1
+# Create output directory
+OUT_DIR=results/hippocampus/chr_$SLURM_ARRAY_TASK_ID
+mkdir -p $OUT_DIR
 
-cd ../SuSiEx/examples
-../bin/SuSiEx \
-	--sst_file=AFR.sumstats.txt \
-	--n_gwas=50000 \
-	--ref_file=/projects/b1213/users/alexis/projects/dna-methylation-heritability/heritability/caudate/_m/plink_format/chr_1/TOPMed_LIBD.AA.998311_998546 \
-	--ld_file=../../_m/cal_ld/chr_1/TOPMed_LIBD.AA.998311_998546 \
-	--out_dir=../../_m/results/chr_1 \
-	--out_name=TOPMed_LIBD.AA.998311_998546.output \
-	--level=0.95 \
-	--pval_thresh=1e-5 \
-	--maf=0.005 \
-	--chr=1 \
-	--bp=498311,1498546 \
-	--snp_col=2 \
-	--chr_col=1 \
-	--bp_col=3 \
-	--a1_col=4 \
-	--a2_col=5 \
-	--eff_col=6 \
-	--se_col=7 \
-	--pval_col=9 \
-	--plink=../utilities/plink \
-	--mult-step=True \
-	--keep-ambig=True \
-	--threads=16
+# Define chromosome directory and reference directory
+CHR_DIR="./ld_matrices/hippocampus/chr_${SLURM_ARRAY_TASK_ID}"
+
+for dir in "$CHR_DIR"/*/; do
+    dir_name=$(basename "$dir")
+
+    region=($(echo "$dir_name" | grep -oE '[0-9]+'))
+
+    if [ "${#region[@]}" -eq 2 ]; then
+        START=${region[0]}
+        END=${region[1]}
+        echo "$dir_name: START=$START, END=$END"
+    else
+        echo "$dir_name: numbers found = ${region[*]}"
+    fi
+
+	START_POS=$((START - 500000))
+	END_POS=$((END + 500000))
+
+	../SuSiEx/bin/SuSiEx \
+		--sst_file=../MDD_harmonized.sumstats.txt \
+		--n_gwas=50000 \
+		--ref_file=./ld_matrices/hippocampus/chr_${SLURM_ARRAY_TASK_ID}/${START}_${END}/TOPMed_LIBD.AA.${START}_${END} \
+		--ld_file=./ld_matrices/hippocampus/chr_${SLURM_ARRAY_TASK_ID}/${START}_${END}/TOPMed_LIBD.AA.${START}_${END} \
+		--out_dir=$OUT_DIR \
+		--out_name=TOPMed_LIBD.AA.${START}_${END}.output \
+		--level=0.95 \
+		--pval_thresh=1e-5 \
+		--maf=0.005 \
+		--chr=${SLURM_ARRAY_TASK_ID} \
+		--bp=$START_POS,$END_POS \
+		--snp_col=2 \
+		--chr_col=1 \
+		--bp_col=3 \
+		--a1_col=4 \
+		--a2_col=5 \
+		--eff_col=6 \
+		--se_col=7 \
+		--pval_col=9 \
+		--plink=../utilities/plink \
+		--mult-step=True \
+		--keep-ambig=True \
+		--threads=16
+done
 
 log_message "**** Job ends ****"
