683db9c86d8d77838b38270a0407eaaf865a5737
Alexis Bennett <alexis.bennett@northwestern.edu>
2026-02-03 13:31:53 -0600

    Update to loop through all option flags and heritability categories

--- heritability/elastic_net_model/tissue_comparison/upset_plot/_h/02.plot_upset.R
+++ heritability/elastic_net_model/tissue_comparison/upset_plot/_h/02.plot_upset.R
@@ -2,11 +2,10 @@
 
 suppressPackageStartupMessages({
   library(data.table)
-  library(magrittr)
-  library(ComplexHeatmap)
+  library(here)
   library(UpSetR)
   library(ggplot2)
-  library(ComplexUpset)
+  #library(ComplexUpset)
   library(tibble)
 })
 
@@ -19,32 +18,49 @@ count_intersections <- function(fn){
 # Formatting
 
 # Plot Upset
-plot_upset <- function(sets){
-  pdf("VMR_upsetR_plot.pdf", width = 6, height = 4)
-  UpSetR::upset(
-    fromExpression(sets),
-    order.by = "freq",
-    sets = c("Caudate", "DLPFC", "Hippocampus")
-  )
-  dev.off()
+plot_upset <- function(sets, fn, w, h){
+  for(ext in c('.pdf')){
+    pdf(file=paste0(fn, ext), width=w, height=h)
+    grid::grid.newpage()
+    UpSetR::upset(
+      fromExpression(sets),
+      order.by = "freq",
+      sets = c("Caudate", "DLPFC", "Hippocampus")
+    )
+    dev.off()
+  }
 }
 
 # Testing complex upset
-df <- as.data.frame(fromExpression(sets))
-ComplexUpset::upset(df, intersect = c("Caudate", "DLPFC", "Hippocampus"))
+#df <- as.data.frame(fromExpression(sets))
+#ComplexUpset::upset(df, intersect = c("Caudate", "DLPFC", "Hippocampus"))
 
 ## Main
-sets <- c(
-  Caudate = count_intersections("./f_0.25/sets/caudate_specific.bed"),
-  DLPFC = count_intersections("./f_0.25/sets/dlpfc_specific.bed"),
-  Hippocampus = count_intersections("./f_0.25/sets/hippocampus_specific.bed"),
-  `Caudate&Hippocampus` = count_intersections("./f_0.25/sets/caudate_hippocampus_overlap.bed"),
-  `Caudate&DLPFC` = count_intersections("./f_0.25/sets/caudate_dlpfc_overlap.bed"),
-  `Hippocampus&DLPFC` = count_intersections("./f_0.25/sets/hippocampus_dlpfc_overlap.bed"),
-  `Caudate&Hippocampus&DLPFC` = count_intersections("./f_0.25/sets/3tissues_overlap.bed.tmp")
-)
+out_path <- here("heritability/elastic_net_model/tissue_comparison/upset_plot/_m")
+if (!dir.exists(out_path)) {
+  dir.create(out_path, recursive = TRUE)
+}
 
-plot_upset(sets)
+option_flags <- c("F_0.25", "F_0.5", "F_0.75", "f_0.25", "f_0.5", "f_0.75")
+
+for (flag in option_flags) {
+  
+  # Get overlapping VMRs
+  sets <- c(
+    Caudate = count_intersections(paste0("./", flag, "/sets/caudate_specific.bed")),
+    DLPFC = count_intersections(paste0("./", flag, "/sets/dlpfc_specific.bed")),
+    Hippocampus = count_intersections(paste0("./", flag, "/sets/hippocampus_specific.bed")),
+    `Caudate&Hippocampus` = count_intersections(paste0("./", flag, "/sets/caudate_hippocampus_overlap.bed")),
+    `Caudate&DLPFC` = count_intersections(paste0("./", flag, "/sets/caudate_dlpfc_overlap.bed")),
+    `Hippocampus&DLPFC` = count_intersections(paste0("./", flag, "/sets/hippocampus_dlpfc_overlap.bed")),
+    `Caudate&Hippocampus&DLPFC` = count_intersections(paste0("./", flag, "/sets/3tissues_overlap.bed.tmp"))
+  )
+  
+  # Plot upset
+  out_upset <- file.path(out_path, flag, "VMR_upsetR")
+  plot_upset(sets, out_upset, 6, 4)
+
+}
 
 #### Reproducibility information
 Sys.time()
