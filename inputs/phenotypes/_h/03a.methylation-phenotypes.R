d8d2858ecae568fa2b7ca1c000624e5c35164351
KrotosBenjamin <kj.benjamin90@gmail.com>
2026-02-05 15:06:15 -0600

    adding new submission script and update methylation phenotype extraction for the new location of data

--- inputs/phenotypes/_h/03a.methylation-phenotypes.R
+++ inputs/phenotypes/_h/03a.methylation-phenotypes.R
@@ -1,8 +1,19 @@
 ## This script extracts phenotype information from the
 ## BSseq objects (DNAm).
 
-load_methyl <- function(fn) {
+load_methyl_rda <- function(fn) {
     load(here::here("inputs/wgbs-data", fn))
+    tissue      <- dirname(fn)
+    sample_data <- bsseq::pData(BSobj) |>
+        as.data.frame() |> dplyr::filter(race %in% c("AA", "CAUC")) |>
+        dplyr::mutate_if(is.character, as.factor) |>
+        dplyr::mutate(region = tissue)
+    sample_data$race <- gsub("CAUC", "EA", sample_data$race)
+    return(sample_data)
+}
+
+load_methyl_h5 <- function(fn) {
+    BSobj       <- HDF5Array::loadHDF5SummarizedExperiment(dir = fn)
     tissue      <- dirname(fn)
     sample_data <- bsseq::pData(BSobj) |>
         as.data.frame() |> dplyr::filter(race %in% c("AA", "CAUC")) |>
@@ -14,13 +25,17 @@ load_methyl <- function(fn) {
 
 #### MAIN
                                         # Load methylation
-fnames  <- c("caudate/Caudate_chr21_BSobj.rda",
-             "hippocampus/Hippocampus_chr21BSobj_Genotypes.rda",
-             "dlpfc/dlpfc_chr21BSobj_GenotypesDosage.rda")
-
-purrr::map_dfr(fnames, load_methyl) |>
+fn1    <- "caudate/_m/caudate_chr21_BSobj.rda"
+fnames <- c("hippocampus/_m/hippocampus_chr21_BSobj",
+            "dlpfc/_m/dlpfc_chr21_BSobj")
+df1 <- load_methyl_rda(fn1) |>
     dplyr::select(c("brnum", "agedeath", "sex", "race", "primarydx",
-                    "pmi", "batch", "region")) |>
+                    "pmi", "batch", "region"))
+df2 <- purrr::map_dfr(fnames, load_methyl_h5) |>
+    dplyr::select(c("brnum", "agedeath", "sex", "race", "primarydx",
+                    "pmi", "batch", "region"))
+
+dplyr::bind_rows(df1, df2) |>
     data.table::fwrite("phenotypes-DNAm-all.tsv", sep="\t")
 
 #### Reproducibility information
