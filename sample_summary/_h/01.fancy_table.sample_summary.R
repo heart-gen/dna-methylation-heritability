fdc5eab5898afdfb3225cb5c3557c745e271413d
Alexis Bennett <alexis.bennett@northwestern.edu>
2026-02-06 11:49:24 -0600

    Add summary table for all samples

--- sample_summary/_h/01.fancy_table.sample_summary.R
+++ sample_summary/_h/01.fancy_table.sample_summary.R
@@ -4,6 +4,7 @@ suppressPackageStartupMessages({
     library(here)
     library(dplyr)
     library(gtsummary)
+    library(data.table)
 })
 
 options(gt.html_tag_check = FALSE)
@@ -17,7 +18,6 @@ save_table <- function(pp, fn){
 
 clean_pheno <- function(pheno_file_path){
   pheno_df <- fread(pheno_file_path, header = TRUE) |>
-    select(agedeath, sex, primarydx, region) |>
     filter(agedeath > 17) |>
     mutate(region = recode(region,
                            "caudate" = "Caudate",
@@ -30,15 +30,17 @@ clean_pheno <- function(pheno_file_path){
 
 ## Main
                                         # Generate phenotype data
-pheno_file_path <- here("inputs/phenotypes/_m/phenotypes-AA.tsv")
+pheno_file_path <- here("inputs/phenotypes/_m/phenotypes-all.tsv")
 pheno_df <- clean_pheno(pheno_file_path)
 
 data.table::fwrite(pheno_df, file="phenotype_data.tsv", sep="\t",
                    row.names=FALSE, col.names=TRUE)
 
                                          # Generate pretty tables
-fn <- "sample_breakdown.table"
-pp <- pheno_df |>
+fn_AA <- "sample_breakdown_AA.table"
+pp_AA <- pheno_df |>
+    filter(race == "AA") |>
+    select(agedeath, sex, primarydx, region) |>
     mutate(sex = factor(sex, labels=c("Female", "Male")),
            primarydx = factor(forcats::fct_drop(primarydx), labels=c("CTL", "SCZ"))) |>
     tbl_summary(by="region", missing="no",
@@ -47,8 +49,26 @@ pp <- pheno_df |>
     modify_header(all_stat_cols() ~ "**{level}**<br>N = {n}") |>
     modify_spanning_header(all_stat_cols() ~ "**Brain Region**") |>
     bold_labels() |> italicize_levels()
-print(pp)
-save_table(pp, fn)
+print(pp_AA)
+save_table(pp_AA, fn_AA)
+
+fn_all <- "sample_breakdown_all.table"
+pp_all <- pheno_df |>
+  select(agedeath, race, sex, primarydx, region) |>
+  mutate(sex = factor(sex, labels=c("Female", "Male")),
+         primarydx = factor(forcats::fct_drop(primarydx), labels=c("CTL", "SCZ"))) |>
+  tbl_strata(
+    strata=region,
+    ~.x %>% 
+      tbl_summary(by="race", missing="no",
+                  label = list(agedeath ~ "Age", sex ~ "Sex", primarydx ~ "Dx"),
+                  statistic=all_continuous() ~ c("{mean} ({sd})")) |>
+      modify_header(all_stat_cols() ~ "**{level}**<br>N = {n}") |>
+      modify_spanning_header(all_stat_cols() ~ "**Brain Region**") |>
+      bold_labels() |> italicize_levels()
+  )
+print(pp_all)
+save_table(pp_all, fn_all)
 
 #### Reproducibility information
 Sys.time()
