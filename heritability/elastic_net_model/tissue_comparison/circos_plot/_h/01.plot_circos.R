e6fd92cd76ebf9741b232af8f868f05d9ff0c36e
Alexis Bennett <alexis.bennett@northwestern.edu>
2026-02-25 13:31:43 -0600

    Update to grayscale; fix legend and sizing

--- heritability/elastic_net_model/tissue_comparison/circos_plot/_h/01.plot_circos.R
+++ heritability/elastic_net_model/tissue_comparison/circos_plot/_h/01.plot_circos.R
@@ -39,8 +39,14 @@ plot_circos_3tissue <- function(caudate, dlpfc, hippo, col, ylim){
                         type="points", legend_gp=gpar(col = c("#7372A6", "#B36F61", "#C5AC47")),
                         title_position="topleft", title="Tissue",
                         background="#FFFFFF")
+    lgd_threshold <- Legend(
+      labels = expression("h"^2*" = 0.1 threshold"),
+      type = "lines",
+      legend_gp = gpar(lty = 2, col = "#6E6E6E"),
+      background = "#FFFFFF"
+    )
     circos.clear() # clear plot if there is any
-    circos.par("start.degree" = 0, "cell.padding" = c(0, 0, 0, 0),
+    circos.par("start.degree" = 90, "cell.padding" = c(0, 0, 0, 0),
                "track.height" = 0.15, gap.degree = gap) # rotate 90 degrees
     # initialize with ideogram
     # use hg38, default is hg19
@@ -49,7 +55,7 @@ plot_circos_3tissue <- function(caudate, dlpfc, hippo, col, ylim){
                         bg.col=add_transparency("#7372A6", transparency=0.8),
                         panel.fun = function(region, value, ...) {
                           circos.genomicLines(region, value,
-                                              type = "segment", col = col, lwd = 2, ...)
+                                              type = "segment", col = col, lwd = 3, ...)
                           circos.lines(CELL_META$cell.xlim, c(0.1, 0.1), lty = 2, col = "black")
                   			  circos.yaxis(labels.cex = 0.6, side = "left", sector.index = "chr1")
     })
@@ -57,7 +63,7 @@ plot_circos_3tissue <- function(caudate, dlpfc, hippo, col, ylim){
                         bg.col=add_transparency("#B36F61", transparency=0.8),
                         panel.fun = function(region, value, ...) {
                           circos.genomicLines(region, value,
-                                              type = "segment", lwd = 2, col = col, ...)
+                                              type = "segment", col = col, lwd = 3, ...)
                           circos.lines(CELL_META$cell.xlim, c(0.1, 0.1), lty = 2, col = "black")
                           circos.yaxis(labels.cex = 0.6, side = "left", sector.index = "chr1")
     })
@@ -65,11 +71,12 @@ plot_circos_3tissue <- function(caudate, dlpfc, hippo, col, ylim){
                         bg.col=add_transparency("#C5AC47", transparency=0.8),
                         panel.fun = function(region, value, ...) {
                           circos.genomicLines(region, value,
-                                              type = "segment", lwd = 2, col = col, ...)
+                                              type = "segment", col = col, lwd = 3, ...)
                           circos.lines(CELL_META$cell.xlim, c(0.1, 0.1), lty = 2, col = "black")
                           circos.yaxis(labels.cex = 0.6, side = "left", sector.index = "chr1")
     })
-    draw(lgd_points, x=unit(5, "mm"), y=unit(5, "mm"), just=c("left", "bottom"))
+    lgd_combined <- packLegend(lgd_points, lgd_threshold, direction = "vertical")
+    draw(lgd_combined, x=unit(5, "mm"), y=unit(5, "mm"), just=c("left", "bottom"))
 }
 
                                         # create output dir
@@ -89,9 +96,9 @@ main <- function(){
                                         # define h2 categories and params
     categories <- c("Heritable", "Non-heritable", "Low prediction")
     category_colors <- list(
-      "Heritable"      = "#497C8A",
-      "Non-heritable"  = "#8CA77B",
-      "Low prediction" = "#E3A27F"
+      "Heritable"      = "#6E6E6E",
+      "Non-heritable"  = "#6E6E6E",
+      "Low prediction" = "#6E6E6E"
     )
     category_ylims <- list(
       "Heritable"      = c(0, 1),
@@ -107,7 +114,7 @@ main <- function(){
       
       pdf(file = file.path(output_path, 
                            paste0("free_y_circos_plot_3regions_", gsub(" ", "_", tolower(category)), ".pdf")),
-          width = 10, height = 10)
+          width = 8, height = 8)
       plot_circos_3tissue(caudate_cat, dlpfc_cat, hippo_cat, 
                           col  = category_colors[[category]],
                           ylim = category_ylims[[category]])
