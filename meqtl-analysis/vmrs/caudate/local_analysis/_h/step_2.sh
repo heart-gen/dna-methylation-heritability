03e88b9e5dab982f215eac869f54245432db6b1b
Elisa Johnson <elisajohnson2027@u.northwestern.edu>
2026-02-27 13:28:39 -0600

    Adds gpu command

--- meqtl-analysis/vmrs/caudate/local_analysis/_h/step_2.sh
+++ meqtl-analysis/vmrs/caudate/local_analysis/_h/step_2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=b1042
 #SBATCH --partition=genomics-gpu
-#SBATCH --gres=gpu:h100:1
+#SBATCH --gres=gpu:a100:1
 #SBATCH --job-name=perm_mapping
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=elisajohnson2027@u.northwestern.edu
