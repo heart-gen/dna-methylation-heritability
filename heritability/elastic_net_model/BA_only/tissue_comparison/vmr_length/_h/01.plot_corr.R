5f6525a94c4dc19f0f822d299631bc5369921d1b
Alexis Bennett <alexis.bennett@northwestern.edu>
2026-03-20 15:02:43 -0500

    Add vmr length scripts for updated r2 thresh

---
+++ heritability/elastic_net_model/BA_only/tissue_comparison/vmr_length/_h/01.plot_corr.R
@@ -0,0 +1,221 @@
+#### Correlate VMR length to heritability estimates ####
+
+suppressPackageStartupMessages({
+  library(here)
+  library(dplyr)
+  library(ggpubr)
+  library(ggplot2)
+})
+
+## Function
+filter_sites <- function(enet) {
+  vmr <- na.omit(enet)
+  vmr <- vmr %>%
+    mutate(h2_category = case_when(
+      r_squared_cv <= 0.3 ~ "Low prediction",
+      h2_unscaled < 0.1 & r_squared_cv > 0.3 ~ "Non-heritable",
+      h2_unscaled >= 0.1 & r_squared_cv > 0.3 ~ "Heritable"
+    ),
+    h2_category = factor(h2_category, levels = c("Heritable", 
+                                                 "Non-heritable", 
+                                                 "Low prediction"))
+    )
+  return(vmr)
+}
+
+cal_vmr_length <- function(vmr) {
+  vmr <- vmr %>%
+    mutate(length = end - start,
+           log10_length = log10(length))
+  return(vmr)
+}
+
+get_long_vmrs <- function(vmr, tissue, out_path) {
+  vmr_long <- vmr %>%
+    filter(length > 5000) %>%
+    select(chrom, start, end)
+  
+  write.table(vmr_long,
+              file = file.path(out_path, paste0("long_vmrs_", tissue, ".bed")),
+              sep = "\t", quote = FALSE, row.names = FALSE, col.names = FALSE)
+  return(vmr_long)
+}
+
+summarise_length <- function(vmr, tissue, out_path) {
+  summary_df <- vmr %>%
+    group_by(h2_category) %>%
+    summarise(
+      n = n(),
+      mean_length = mean(length, na.rm = TRUE),
+      median_length = median(length, na.rm = TRUE),
+      .groups = "drop"
+    )
+  print(summary_df)
+  write.csv(summary_df, 
+            file = file.path(out_path, 
+                             paste0("vmr_length_summary_", tissue, ".csv")), 
+            row.names = FALSE)
+}
+
+spearman_corr <- function(vmr, tissue, out_path) {
+  spearman <- vmr %>% 
+    group_by(h2_category) %>%
+    summarise(
+      spearman_rho = cor.test(log10_length, h2_unscaled, method = "spearman")$estimate,
+      spearman_p_value = cor.test(log10_length, h2_unscaled, method = "spearman")$p.value,
+      n = n()
+    )
+  print(spearman)
+  write.csv(spearman, 
+            file = file.path(out_path, 
+                             paste0("vmr_length_h2_corr_", tissue, ".csv")), 
+            row.names = FALSE)
+}
+
+save_plot <- function(p, fn, w, h){
+  for(ext in c('.png', '.pdf')){
+    ggsave(file=paste0(fn, ext), plot=p, width=w, height=h)
+  }
+}
+
+plot_density <- function(vmr, tissue) {
+  # Define palette
+  category_colors <- c(
+    "Heritable" = "#497C8A",
+    "Non-heritable" = "#8CA77B",
+    "Low prediction" = "#E3A27F"
+  )
+  
+  counts <- vmr %>%
+    group_by(h2_category) %>%
+    summarise(n = n(), .groups = "drop")
+  
+  labels <- setNames(
+    paste0(counts$h2_category, "\n(n = ", counts$n, ")"),
+    counts$h2_category
+  )
+  
+  tissue_title <- ifelse(tolower(tissue) == "dlpfc", "DLPFC", tools::toTitleCase(tissue))
+  
+  p_hist <- gghistogram(vmr, x = "log10_length", 
+                        add_density = TRUE, rug = TRUE, 
+                        add = "median",
+                        color = "h2_category", fill = "h2_category",
+                        ggtheme = theme_pubr(base_size = 15, border = TRUE),
+                        xlab = "log10(VMR Length)", ylab = "Count") +
+    facet_wrap(~h2_category, labeller = as_labeller(labels), scales = "free_x") +
+    scale_color_manual(values = category_colors) +
+    scale_fill_manual(values = category_colors) +
+    ggtitle(paste("VMR length distribution:", tissue_title)) +
+    labs(color = NULL, fill = NULL) +
+    font("xy.title", face = "bold", size = 14) +
+    theme(
+      axis.text.x = element_text(angle = 45, hjust = 1),
+      plot.title = element_text(hjust = 0.5)
+    )
+  
+  return(p_hist)
+}
+
+plot_corr <- function(vmr, tissue) {
+  counts <- vmr %>%
+    group_by(h2_category) %>%
+    summarise(n = n(), .groups = "drop")
+  
+  labels <- setNames(
+    paste0(counts$h2_category, "\n(n = ", counts$n, ")"),
+    counts$h2_category
+  )
+  
+  # Define palette
+  category_colors <- c(
+    "Heritable" = "#497C8A",
+    "Non-heritable" = "#8CA77B",
+    "Low prediction" = "#E3A27F"
+  )
+  
+  tissue_title <- ifelse(tolower(tissue) == "dlpfc", "DLPFC", tools::toTitleCase(tissue))
+  
+  p_corr <- ggscatter(vmr, x = "log10_length", y = "h2_unscaled",
+                      add = "reg.line", size = 1, alpha = 0.75,
+                      xlab = "log10(VMR Length)", ylab = "Estimated h2", 
+                      conf.int = TRUE,
+                      cor.coef = TRUE, cor.coef.size = 4,
+                      cor.coeff.args = list(
+                        label.sep = "\n",
+                        label.x.npc = 0.05,
+                        label.y.npc = 0.95
+                      ),
+                      cor.method = "spearman",
+                      color = "h2_category",
+                      add.params = list(fill = "lightgray", alpha = 0.75),
+                      ggtheme = theme_pubr(base_size = 15, border = TRUE)
+  ) +
+    facet_wrap(~h2_category, labeller = as_labeller(labels), scales = "free_x") +
+    scale_color_manual(values = category_colors) +
+    scale_fill_manual(values = category_colors) +
+    labs(color = NULL) +
+    ggtitle(paste("VMR length correlation:", tissue_title)) +
+    font("xy.title", face = "bold", size = 14) +
+    theme(
+      axis.text.x = element_text(angle = 45, hjust = 1),
+      plot.title = element_text(hjust = 0.5)
+    ) +
+    coord_cartesian(ylim = c(0, 1)) +
+    geom_hline(yintercept = 0.1, linetype = "dashed", color = "#2A0F07")
+  return(p_corr)
+}
+
+## Main
+tissues <- c("caudate", "hippocampus", "dlpfc")
+
+out_path <- here("heritability/elastic_net_model/BA_only/tissue_comparison/vmr_length/_m")
+if (!dir.exists(out_path)) {
+  dir.create(out_path, recursive = TRUE)
+}
+
+hist_plots <- list()
+corr_plots <- list()
+
+for (tissue in tissues) {
+  # Read in summary table
+  enet_file <- here("heritability/elastic_net_model/BA_only/", 
+                    paste0(tissue, "/_m/", tissue, "_summary_elastic-net.tsv"))
+  enet <- read.table(enet_file, sep = "\t", header = TRUE)
+  
+  vmr <- filter_sites(enet)
+  vmr <- cal_vmr_length(vmr)
+  
+  # Save VMRs > 5000bp
+  vmr_long <- get_long_vmrs(vmr, tissue, out_path)
+  
+  # Summarize length of vmrs
+  summarise_length(vmr, tissue, out_path)
+
+  # Spearman correlation test 
+  spearman_corr(vmr, tissue, out_path)
+
+  # Plotting
+  p_hist <- plot_density(vmr, tissue)
+  p_corr <- plot_corr(vmr, tissue)
+  
+  # Store plots
+  hist_plots[[tissue]] <- p_hist
+  corr_plots[[tissue]] <- p_corr
+}
+
+# Save plots
+combined_hist <- ggarrange(plotlist = hist_plots, ncol = 3, nrow = 1)
+combined_corr <- ggarrange(plotlist = corr_plots, ncol = 3, nrow = 1)
+
+fn_hist <- file.path(out_path, "VMR_length_distribution")
+fn_corr <- file.path(out_path, "VMR_length_h2_correlation")
+save_plot(combined_hist, fn_hist, 20, 6)
+save_plot(combined_corr, fn_corr, 20, 6)
+
+#### Reproducibility information ####
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+sessioninfo::session_info()
---
+++ heritability/elastic_net_model/BA_only/tissue_comparison/vmr_length/_h/02.sig_length.R
@@ -0,0 +1,195 @@
+#### Check for length normality and significance ####
+
+suppressPackageStartupMessages({
+  library(here)
+  library(dplyr)
+  library(rstatix)
+  library(ggpubr)
+  library(ggplot2)
+  library(lme4)
+  library(emmeans)
+  library(data.table)
+})
+
+## Function 
+filter_sites <- function(enet) {
+  vmr <- na.omit(enet)
+  vmr <- vmr %>%
+    mutate(h2_category = case_when(
+      r_squared_cv <= 0.3 ~ "Low prediction",
+      h2_unscaled < 0.1 & r_squared_cv > 0.3 ~ "Non-heritable",
+      h2_unscaled >= 0.1 & r_squared_cv > 0.3 ~ "Heritable"
+    ),
+    h2_category = factor(h2_category, levels = c("Heritable", 
+                                                 "Non-heritable", 
+                                                 "Low prediction"))
+    )
+  return(vmr)
+}
+
+cal_vmr_length <- function(vmr) {
+  vmr <- vmr %>%
+    mutate(length = end - start,
+           log10_length = log10(length))
+  return(vmr)
+}
+
+get_samples <- function(tissue) {
+  sample_fn <- here("vmr-analysis/", paste0(tissue, "/_m/samples.txt"))
+  samples <- fread(sample_fn, header = FALSE)
+  n_samples <- nrow(samples)
+  return(n_samples)
+}
+
+save_plot <- function(p, fn, w, h){
+  for(ext in c('.png', '.pdf')){
+    ggsave(file=paste0(fn, ext), plot=p, width=w, height=h)
+  }
+}
+
+## Main
+tissues <- c("caudate", "hippocampus", "dlpfc")
+
+out_path <- here("heritability/elastic_net_model/BA_only/tissue_comparison/vmr_length/_m")
+if (!dir.exists(out_path)) {
+  dir.create(out_path, recursive = TRUE)
+}
+
+vmr_all    <- list()
+
+for (tissue in tissues) {
+  # Read in summary table
+  enet_file <- here("heritability/elastic_net_model/BA_only/", 
+                    paste0(tissue, "/_m/", tissue, "_summary_elastic-net.tsv"))
+  enet <- read.table(enet_file, sep = "\t", header = TRUE)
+  
+  vmr <- filter_sites(enet)
+  vmr <- cal_vmr_length(vmr)
+  
+  # Store vmrs across all tissues
+  vmr$tissue <- tissue
+  vmr_all[[tissue]] <- vmr
+}
+
+vmr_all <- bind_rows(vmr_all)
+
+# Test significance with mixed linear model
+contrasts_h2 <- list()
+
+for(t in unique(vmr_all$tissue)) {
+  
+  n_samples <- get_samples(t)
+  df <- vmr_all %>% filter(tissue == t) %>%
+    mutate(n_samples = n_samples)
+  
+  # Fit mixed model per tissue
+  lmm <- lmer(log10_length ~ h2_category + n_samples + (1 | chrom), data = df)
+  y_pos = max(df$log10_length, na.rm = TRUE) + 0.4
+  
+  # Get pairwise contrasts
+  contrast_df <- emmeans(lmm, ~ h2_category) %>%
+    contrast(method = "pairwise", adjust = "bonferroni") %>%
+    as.data.frame() %>%
+    mutate(
+      tissue = t,
+      group1 = gsub("[()]", "", sapply(strsplit(contrast, " - "), `[`, 1)),
+      group2 = gsub("[()]", "", sapply(strsplit(contrast, " - "), `[`, 2)),
+      y.position = y_pos,
+      p.adj.signif = case_when(
+        p.value <= 0.001 ~ "***",
+        p.value <= 0.01  ~ "**",
+        p.value <= 0.05  ~ "*",
+        TRUE ~ "ns"
+      )
+    )
+  
+  contrasts_h2[[t]] <- contrast_df
+}
+
+heritability_test <- bind_rows(contrasts_h2)
+
+heritability_colors <- c(
+  "Heritable" = "#497C8A",
+  "Non-heritable" = "#8CA77B",
+  "Low prediction" = "#E3A27F"
+)
+
+p_heritability <- ggviolin(vmr_all, x = "h2_category",
+                           y = "log10_length", fill = "h2_category", 
+                           facet.by = "tissue", add = "boxplot", 
+                           alpha = 0.5, palette = heritability_colors, 
+                           trim = FALSE
+) +
+  stat_pvalue_manual(heritability_test, label = "p.adj.signif", tip.length = 0.01) +
+  theme_pubr(base_size = 15, border = TRUE) +
+  labs(
+    title = "VMR Length Differences Across Heritability Categories",
+    x = "Tissue", y = "log10(VMR Length)"
+  ) +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1),
+    plot.title = element_text(hjust = 0.5)
+  )
+
+contrasts_tissue <- list()
+
+for(h in unique(vmr_all$h2_category)) {
+  
+  n_samples <- get_samples(t)
+  df <- vmr_all %>% filter(h2_category == h) %>%
+    mutate(n_samples = n_samples)
+  
+  # Fit mixed model per h2 category 
+  lmm <- lmer(log10_length ~ tissue + n_samples + (1 | chrom), data = df)
+  y_pos = max(df$log10_length, na.rm = TRUE) + 0.4
+  
+  # Get pairwise contrasts
+  contrast_df <- emmeans(lmm, ~ tissue) %>%
+    contrast(method = "pairwise", adjust = "bonferroni") %>%
+    as.data.frame() %>%
+    mutate(
+      h2_category = h,
+      group1 = gsub("[()]", "", sapply(strsplit(contrast, " - "), `[`, 1)),
+      group2 = gsub("[()]", "", sapply(strsplit(contrast, " - "), `[`, 2)),
+      y.position = y_pos,
+      p.adj.signif = case_when(
+        p.value <= 0.001 ~ "***",
+        p.value <= 0.01  ~ "**",
+        p.value <= 0.05  ~ "*",
+        TRUE ~ "ns"
+      )
+    )
+  
+  contrasts_tissue[[h]] <- contrast_df
+}
+
+tissue_test <- bind_rows(contrasts_tissue)
+
+tissue_colors <- c(
+  "caudate" = "#7372A6",
+  "dlpfc" = "#B36F61",
+  "hippocampus" = "#C5AC47"
+)
+
+p_tissue <- ggviolin(vmr_all, x = "tissue",
+                     y = "log10_length", fill = "tissue",
+                     facet.by = "h2_category", add = "boxplot", 
+                     alpha = 0.5, palette = tissue_colors, 
+                     trim = FALSE
+) +
+  stat_pvalue_manual(tissue_test, label = "p.adj.signif", tip.length = 0.01) +
+  theme_pubr(base_size = 15, border = TRUE) +
+  labs(
+    title = "VMR Length Differences Across Tissues",
+    x = "Tissue", y = "log10(VMR Length)"
+  ) +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1),
+    plot.title = element_text(hjust = 0.5)
+  )
+
+fn_heritability <- file.path(out_path, "VMR_length_heritability_comparisons")
+fn_tissue       <- file.path(out_path, "VMR_length_tissue_comparisons")
+save_plot(p_tissue, fn_tissue, 14, 8)
+save_plot(p_heritability, fn_heritability, 14, 8)
+
