44b2e3394bee44b340bd80f22591ed56df3ac523
Alexis Bennett <alexis.bennett@northwestern.edu>
2026-03-25 16:42:59 -0500

    Add functions for repetitive code

--- sensitivity-analysis/BA_only/_h/03.plot_sankey.R
+++ sensitivity-analysis/BA_only/_h/03.plot_sankey.R
@@ -15,70 +15,30 @@ save_plot <- function(p, fn, w, h, dpi){
   }
 }
 
-## Main 
-# Create output directory
-out_path <- here("sensitivity-analysis/BA_only/_m")
-if (!dir.exists(out_path)) {
-  dir.create(out_path, recursive = TRUE)
+get_summary <- function(enet_all, r2_threshold){
+  enet_cat <- enet_all %>%
+    mutate(h2_category = case_when(
+             r_squared_cv <= r2_threshold ~ "Low prediction",
+             h2_unscaled < 0.1 & r_squared_cv > r2_threshold ~ "Non-heritable",
+             h2_unscaled >= 0.1 & r_squared_cv > r2_threshold ~ "Heritable"
+           ),
+           r2_threshold = r2_threshold)
+
+  enet_cat <- enet_cat %>%
+    mutate(h2_category = factor(h2_category,
+                                levels = c("Heritable", "Non-heritable", "Low prediction")),
+           region = recode(region, "caudate" = "Caudate"))
+
+  return(enet_summary)
 }
 
-# Read in and filter summary tables
-tissues <- c("caudate", "hippocampus", "dlpfc")
-
-enet_list <- list()
-
-for (tissue in tissues) {
-    enet_file <- here("heritability/elastic_net_model/all_individuals", tissue, "_m",
-                      paste0(tissue, "_summary_elastic-net.tsv"))
-    enet <- read.table(enet_file, sep = "\t", header = TRUE)
-    enet <- na.omit(enet)
-    enet_list[[paste(tissue, pop, sep = "_")]] <- enet
-}
-
-enet_all <- bind_rows(enet_list)
-
-# Get summary for each r2 threshold
-enet_high <- enet_all %>%
-  mutate(h2_category = case_when(
-    r_squared_cv <= 0.75 ~ "Low prediction",
-    h2_unscaled < 0.1 & r_squared_cv > 0.75 ~ "Non-heritable",
-    h2_unscaled >= 0.1 & r_squared_cv > 0.75 ~ "Heritable"
-  ),
-  r2_threshold = 0.75)
-
-enet_high <- enet_high %>%
-  mutate(h2_category = factor(h2_category,
-                              levels = c("Heritable", "Non-heritable", "Low prediction")),
-         region = recode(region, "caudate" = "Caudate"))
-
-enet_low <- enet_all %>%
-  mutate(h2_category = case_when(
-    r_squared_cv <= 0.3 ~ "Low prediction",
-    h2_unscaled < 0.1 & r_squared_cv > 0.3 ~ "Non-heritable",
-    h2_unscaled >= 0.1 & r_squared_cv > 0.3 ~ "Heritable"
-  ),
-  r2_threshold = 0.3)
-
-enet_low <- enet_low %>%
-  mutate(h2_category = factor(h2_category,
-                              levels = c("Heritable", "Non-heritable", "Low prediction")),
-         region = recode(region, "caudate" = "Caudate"))
-
-# Summarize by r2 threshold
-all_summary <- bind_rows(enet_high, enet_low)
-
-low_pred_summary <- all_summary %>%
-  pivot_wider(
-    names_from = r2_threshold,
-    values_from = h2_category,
-    names_prefix = "r2_"
-  ) %>%
-  filter(r2_0.75 == "Low prediction")
-
+plot_sankey <- function(low_pred_summary){
+                                        # Prepare data for plotting
 summary <- low_pred_summary %>%
   group_by(region, r2_0.75, r2_0.3) %>%
   summarise(count = n(), .groups = "drop")
 
+                                        # Plot sankey 
 p_sankey <- ggplot(summary, aes(axis1 = r2_0.75, 
                                 axis2 = factor(region),
                                 axis3 = r2_0.3, y = count)) +
@@ -110,7 +70,54 @@ p_sankey <- ggplot(summary, aes(axis1 = r2_0.75,
     axis.text.x = element_text(angle = 45, hjust = 1, size = 12)
   )
 
-# Save plot
+  return(p_sankey)
+}
+                        
+## Main 
+                                        # Create output directory
+out_path <- here("sensitivity-analysis/BA_only/_m")
+if (!dir.exists(out_path)) {
+  dir.create(out_path, recursive = TRUE)
+}
+
+                                        # Define tissues
+tissues <- c("caudate", "hippocampus", "dlpfc")
+
+                                        # Initialize list for enet 
+                                        # summaries
+enet_list <- list()
+
+for (tissue in tissues) {
+                                        # Read in elastic net summaries
+    enet_file <- here("heritability/elastic_net_model/all_individuals", tissue, "_m",
+                      paste0(tissue, "_summary_elastic-net.tsv"))
+    enet <- read.table(enet_file, sep = "\t", header = TRUE)
+    enet <- na.omit(enet)
+    enet_list[[paste(tissue, pop, sep = "_")]] <- enet
+}
+                                        # Combine across tissues
+enet_all <- bind_rows(enet_list)
+
+                                        # Get summary for each r2 threshold
+enet_high <- get_summary(enet_all, r2_threshold = 0.75)
+enet_low <- get_summary(enet_all, r2_threshold = 0.3)
+
+                                        # Combine summaries
+all_summary <- bind_rows(enet_high, enet_low)
+
+                                        # Summarize low prediction vmrs 
+                                        # across r2 thresholds
+low_pred_summary <- all_summary %>%
+  pivot_wider(
+    names_from = r2_threshold,
+    values_from = h2_category,
+    names_prefix = "r2_"
+  ) %>%
+  filter(r2_0.75 == "Low prediction")
+
+p_sankey <- plot_sankey(low_pred_summary)
+
+                                        # Save plot
 plot_file <- file.path(out_path, "0.75_to_0.3_sankey")
 save_plot(p_sankey, plot_file, w = 12, h = 6, dpi = 300)
 
