ffe53f3fecce2fafa38e46e7468c2af4ccd4e008
jalilhemphill1 <jalilbones10@gmail.com>
2026-04-06 15:17:35 -0500

    Adding scripts for QC from epiclock

---
+++ qc_analysis/_h/step1.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --account=p32505
+#SBATCH --partition=short
+#SBATCH --job-name=qc
+#SBATCH --mem=100GB
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=jalilhemphill2029@u.northwestern.edu
+#SBATCH --output=logs/summary_data_%j.log
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=00:45:00
+
+log_message() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
+}
+
+log_message "**** Job starts ****"
+
+echo "**** Quest info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOBID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURM_NODENAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "SLURM_ARRAY_TASK_ID: ${SLURM_ARRAY_TASK_ID:-N/A}"
+
+module purge
+module list
+
+ENV_PATH="/projects/p32505/opt/envs"
+
+
+log_message "**** Run summary ****"
+conda run -p "${ENV_PATH}/epigenomics" quarto render ../_h/qc_caudate_final.qmd
+
+log_message "**** Job ends ****"
