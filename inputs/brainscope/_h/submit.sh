b576d6ae6af32cf03729feb209897cd9d6132f1c
KrotosBenjamin <kj.benjamin90@gmail.com>
2026-04-16 06:59:34 -0500

    submission scripts

---
+++ inputs/brainscope/_h/submit.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://brainscope.gersteinlab.org/data/atac_peaks/scCRE.PeakCalls.zip
+wget http://resource.psychencode.org/Datasets/Derived/DER-04b_hg38lft_high_confidence_PEC_enhancers.bed
+wget http://resource.psychencode.org/Datasets/Derived/DER-05_PFC_H3K27ac_peaks.bed
