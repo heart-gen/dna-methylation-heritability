a2b5f4503999ba57b49ce348fe7c2692111dbb4e
KrotosBenjamin <kj.benjamin90@gmail.com>
2026-04-16 06:59:24 -0500

    placeholder script

---
+++ inputs/brainscope/brainspan/_h/submit.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://pengfei-mssm.s3.amazonaws.com/neuron_ChromHMM.bed.gz
+wget https://pengfei-mssm.s3.amazonaws.com/glia_ChromHMM.bed.gz
+wget https://pengfei-mssm.s3.amazonaws.com/fetal_ChromHMM.bed.gz
