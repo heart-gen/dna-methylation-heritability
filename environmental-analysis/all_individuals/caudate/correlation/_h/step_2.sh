9469cfb6516bcdba82c79df19c07e236a2d1b7c1
Alexis Bennett <alexis.bennett@northwestern.edu>
2026-04-23 13:31:07 -0500

    Update submission script

--- environmental-analysis/all_individuals/caudate/correlation/_h/step_2.sh
+++ environmental-analysis/all_individuals/caudate/correlation/_h/step_2.sh
@@ -7,9 +7,9 @@
 #SBATCH --mem=16G                # Memory limit
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=alexis.bennett@northwestern.edu
-#SBATCH --array=0-2
+#SBATCH --array=0,1
 #SBATCH --job-name=extract_dmr # Job name
-#SBATCH --output=logs/extract_dmr.%j.log # Standard output log
+#SBATCH --output=logs/extract_dmr.%A_%a.log # Standard output log
 
 # Log function
 log_message() {
@@ -34,7 +34,7 @@ module list
 # Set path variables
 ENV_PATH="/projects/p32505/opt/envs"
 
-POPULATIONS=("all" "BA" "WA")
+POPULATIONS=("BA" "WA")
 POP=${POPULATIONS[$SLURM_ARRAY_TASK_ID]}
 export population=${POP}
 
