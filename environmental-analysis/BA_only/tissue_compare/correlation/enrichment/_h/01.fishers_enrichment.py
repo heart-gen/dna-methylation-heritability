65f94b3bdd8056386a5ca7eed6a20a0d7442154f
Alexis Bennett <alexis.bennett@northwestern.edu>
2026-04-24 13:48:31 -0500

    Update for h2 continuous analyis

--- environmental-analysis/BA_only/tissue_compare/correlation/enrichment/_h/01.fishers_enrichment.py
+++ environmental-analysis/BA_only/tissue_compare/correlation/enrichment/_h/01.fishers_enrichment.py
@@ -13,7 +13,7 @@ import session_info
 
 BINARY_ENVS = [
     "smoking", "codeine", "morphine", "cocaine", "ethanol",
-    "antipsychotics", "nicotine", "amphetamines", "any_trauma_hx"
+    "nicotine", "amphetamines"
 ]
 
 CATEGORICAL_ENVS = {
@@ -22,39 +22,55 @@ CATEGORICAL_ENVS = {
 }
 
 @lru_cache()
-def get_enet(tissue):
+def get_enet(tissue, n_quintiles=5):
     # get vmrs
     enet_fn = here(f"heritability/elastic_net_model/BA_only/{tissue.lower()}/_m/{tissue.lower()}_summary_elastic-net.tsv")
     df = pd.read_csv(enet_fn, sep='\t')
     df = df.dropna()
     df['chrom'] = 'chr' + df['chrom'].astype(str)
 
-    # assign h2 categories
-    df['h2_category'] = df.apply(
-    lambda row: (
-        'Heritable' if row['h2_unscaled'] >= 0.1 and row['r_squared_cv'] > 0.3 else
-        'Non-heritable' if row['h2_unscaled'] < 0.1 and row['r_squared_cv'] > 0.3 else
-        'Low prediction'
-    ),
-    axis=1
+    # Remove low prediction sites
+    df = df[df['r_squared_cv'] > 0.3]
+
+    # Define h2 quartiles
+    breaks = np.unique(np.quantile(df["h2_unscaled"],
+                                   q=np.linspace(0, 1, n_quintiles + 1)))
+    n_bins = len(breaks) -1 
+
+    if n_bins < 1:
+        raise ValueError("Insufficient unique h2 values to compute quantile bins")
+
+    if n_bins < n_quintiles: 
+        raise ValueError(f"Reduced h2 bins from {n_quintiles} to {n_bins} because quantile breaks were not unique")
+    
+    labels = [f"Q{i}" for i in range(1, n_bins + 1)]
+
+    df["h2_quintile"] = pd.cut(
+        df["h2_unscaled"],
+        bins=breaks,
+        labels=labels,
+        include_lowest=True
     )
+    
+    df = df[df["h2_quintile"].notna()]
+
     return df
 
 @lru_cache()
-def get_vmrs(tissue, env, cat=None):
-    fn = here(f"environmental-analysis/BA_only/{tissue.lower()}/correlation/_m/{env.lower()}_logit.csv.gz")
+def get_vmrs(tissue, quintile, env, cat=None):
+    fn = here(f"environmental-analysis/BA_only/{tissue.lower()}/correlation/_m/{quintile}/{env.lower()}_linear.csv.gz")
     vmrs = pd.read_csv(fn, sep=',') 
 
     if cat is not None:
         vmrs = vmrs[vmrs['var'] == f"{env}{cat}"]
 
-    vmrs['test'] = 'logit'
+    vmrs['test'] = 'lm'
     vmrs['sig'] = vmrs['p'] < 0.05
     return vmrs
 
 @lru_cache()
-def get_dmrs(tissue, env, cat=None):
-    fn = here(f"environmental-analysis/BA_only/{tissue.lower()}/correlation/_m/{env.lower()}_dmr.csv.gz")
+def get_dmrs(tissue, quintile, env, cat=None):
+    fn = here(f"environmental-analysis/BA_only/{tissue.lower()}/correlation/_m/{quintile}/{env.lower()}_dmr.csv.gz")
     dmrs = pd.read_csv(fn, sep=',')
 
     if cat is not None:
@@ -65,60 +81,64 @@ def get_dmrs(tissue, env, cat=None):
     return dmrs
 
 @lru_cache()
-def concat_dataframe(tissue, env, cat=None):
-    return pd.concat([get_vmrs(tissue, env, cat), 
-                      get_dmrs(tissue, env, cat)])
+def concat_dataframe(tissue, quintile, env, cat=None):
+    return pd.concat([get_vmrs(tissue, quintile, env, cat), 
+                      get_dmrs(tissue, quintile, env, cat)])
 
 @lru_cache()
-def merge_dataframe(tissue, env, cat=None):
-    merged = concat_dataframe(tissue, env, cat)
+def merge_dataframe(tissue, quintile, env, cat=None):
+    merged = concat_dataframe(tissue, quintile, env, cat)
     merged['chr'] = 'chr' + merged['chr'].astype(str)
 
     df = merged.pivot_table(values='sig', columns='test', fill_value=0,
                          index=['chr', 'start', 'end'])
     
-    df["both"] = ((df['logit'] == 1) & (df['dmr'] == 1)).astype(int)
+    df["both"] = ((df['lm'] == 1) & (df['dmr'] == 1)).astype(int)
     
     return df.merge(get_enet(tissue), 
                     left_on=['chr', 'start', 'end'],
                     right_on=['chrom', 'start', 'end'],
                     how='left')
 @lru_cache()
-def cal_fishers_annot(tissue, test, h2_cat, env, cat=None):
-    df = merge_dataframe(tissue, env, cat)
+def cal_fishers_annot(tissue, test, quintile, env, cat=None):
+    df = merge_dataframe(tissue, quintile, env, cat)
     
-    table = [[np.sum((df[test.lower()] == 1) & (df['h2_category'] == h2_cat)), 
-              np.sum((df[test.lower()] == 1) & (df['h2_category'] != h2_cat))],
-             [np.sum((df[test.lower()] == 0) & (df['h2_category'] == h2_cat)), 
-              np.sum((df[test.lower()] == 0) & (df['h2_category'] != h2_cat))]]
+    table = [[np.sum((df[test.lower()] == 1) 
+                     & (df['h2_quintile'] == quintile)), 
+              np.sum((df[test.lower()] == 1) 
+                     & (df['h2_quintile'] != quintile))],
+             [np.sum((df[test.lower()] == 0) 
+                     & (df['h2_quintile'] == quintile)), 
+              np.sum((df[test.lower()] == 0) 
+                     & (df['h2_quintile'] != quintile))]]
     print(table)
     return fisher_exact(table)
 
 def calculate_enrichment():
-    region_lt = []; h2_lt = []; test_lt = []; fdr_lt = []; pval_lt = []; oddratio_lt = []; env_lt = []
+    region_lt = []; test_lt = []; fdr_lt = []; pval_lt = []; oddratio_lt = []; env_lt = []; quintile_lt = []
 
-    for tissue in ["Caudate", "Hippocampus", "DLPFC"]:
-        for h2_cat in ["Heritable", "Non-heritable", "Low prediction"]:
+    for quintile in ["Q1", "Q2", "Q3", "Q4", "Q5"]:
+        for tissue in ["Caudate", "Hippocampus", "DLPFC"]:
             pvals = []
-            for test in ["Logit", "DMR", "Both"]:
+            for test in ["LM", "DMR", "Both"]:
                 for env in BINARY_ENVS:
-                    odd_ratio, pval = cal_fishers_annot(tissue, test, h2_cat, env, None)
-                    pvals.append(pval); h2_lt.append(h2_cat)
-                    oddratio_lt.append(odd_ratio); test_lt.append(test)
-                    region_lt.append(tissue); env_lt.append(env)
-                    
+                    odd_ratio, pval = cal_fishers_annot(tissue, test, quintile, env, None)
+                    pvals.append(pval); oddratio_lt.append(odd_ratio) 
+                    test_lt.append(test); region_lt.append(tissue)
+                    env_lt.append(env); quintile_lt.append(quintile)
+                        
                 for env, vars in CATEGORICAL_ENVS.items():
                     for cat in vars:
-                        odd_ratio, pval = cal_fishers_annot(tissue, test, h2_cat, env, cat)
-                        pvals.append(pval); h2_lt.append(h2_cat)
-                        oddratio_lt.append(odd_ratio); test_lt.append(test)
-                        region_lt.append(tissue); env_lt.append(cat)
-                        
+                        odd_ratio, pval = cal_fishers_annot(tissue, test, quintile, env, cat)
+                        pvals.append(pval); oddratio_lt.append(odd_ratio)
+                        test_lt.append(test); region_lt.append(tissue); 
+                        env_lt.append(cat ); quintile_lt.append(quintile)
+                            
             _, fdr = fdrcorrection(pvals) # FDR correction per comparison and version
             pval_lt = np.concatenate((pval_lt, pvals))
             fdr_lt = np.concatenate((fdr_lt, fdr))
     # Generate dataframe
-    return pd.DataFrame({'Tissue': region_lt, 'h2_Category': h2_lt, 
+    return pd.DataFrame({'Tissue': region_lt, "Quintile": quintile_lt,
                          'OR': oddratio_lt, 'PValue': pval_lt, 
                          "FDR": fdr_lt, 'Test': test_lt, 'Env': env_lt})
 
