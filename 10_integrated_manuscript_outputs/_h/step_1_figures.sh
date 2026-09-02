066fdfbcb0ec5d40d242c2a8eba8bab1de06e317
KrotosBenjamin <kj.benjamin90@gmail.com>
2026-09-02 14:58:25 -0500

    Move module 10 figures to the buy-in allocation
    step_1_figures.sh was the only script in the module still on
    --account=p32505 --partition=short while modules 03/04/05 use
    --account=b1042 --partition=genomics --qos=buyin.
    
    Note the name collision: the SLURM partition "genomics" is unrelated to the
    conda environment of the same name.

--- 10_integrated_manuscript_outputs/_h/step_1_figures.sh
+++ 10_integrated_manuscript_outputs/_h/step_1_figures.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-#SBATCH --account=p32505
-#SBATCH --partition=short
+#SBATCH --account=b1042
+#SBATCH --partition=genomics
+#SBATCH --qos=buyin
 #SBATCH --time=02:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
